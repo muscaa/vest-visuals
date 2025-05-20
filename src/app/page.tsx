a09f18aa80a8cb79439048bf87c1407f42a29d65
Musca Mihail <muscamihaill@gmail.com>
2025-05-20 16:19:36 +0300

    added custom color theme, added test card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "@/theme/theme-toggle";
+import { TestCard } from "@/components/test-card";
 
 export default function Home() {
     return (
-        <div className="flex items-center justify-center min-h-screen min-w-screen gap-2">
-            <Button>
-                hello world
-            </Button>
+        <div className="flex flex-col items-center justify-center min-h-screen min-w-screen gap-2">
             <ThemeToggle />
+            <TestCard />
         </div>
     );
 }
