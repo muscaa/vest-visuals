6610920fcc10728391b3ec838752c57f78ef986d
Musca Mihail <muscamihaill@gmail.com>
2025-05-20 12:46:21 +0300

    added theme changer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/theme/theme-toggle";
 
 export default function Home() {
     return (
-        <div className="flex items-center justify-center min-h-screen min-w-screen">
+        <div className="flex items-center justify-center min-h-screen min-w-screen gap-2">
             <Button>
                 hello world
             </Button>
+            <ThemeToggle />
         </div>
     );
 }
