54cf15a8072520226667b31f66768f83dba04879
Musca Mihail <muscamihaill@gmail.com>
2025-05-24 19:41:43 +0300

    minor changes to colors, remove buttons from test card

--- src/components/test-card.tsx
+++ src/components/test-card.tsx
@@ -36,18 +36,6 @@ export function TestCard() {
                 <Button variant="link">link</Button>
                 <Button variant="outline">outline</Button>
                 <Button variant="secondary">secondary</Button>
-                <Button variant="default">default</Button>
-                <Button variant="destructive">destructive</Button>
-                <Button variant="ghost">ghost</Button>
-                <Button variant="link">link</Button>
-                <Button variant="outline">outline</Button>
-                <Button variant="secondary">secondary</Button>
-                <Button variant="default">default</Button>
-                <Button variant="destructive">destructive</Button>
-                <Button variant="ghost">ghost</Button>
-                <Button variant="link">link</Button>
-                <Button variant="outline">outline</Button>
-                <Button variant="secondary">secondary</Button>
             </CardFooter>
         </Card>
     )
