d722ab55b7ed60f68707018ee6b391caa404647c
Musca Mihail <muscamihaill@gmail.com>
2025-05-24 20:22:25 +0300

    added logos

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import Image from "next/image";
+import logo from "@/../public/logos/vest-visuals-slim.svg";
+
 export function Navbar() {
     return (
-        /*<nav className="min-w-screen h-96 bg-secondary">
-
-        </nav>*/
         <nav className="flex flex-col w-full h-16 justify-center items-center bg-secondary relative">
             <div className="flex size-full max-w-6xl justify-between items-center p-2">
-
+                <Image
+                    src={logo}
+                    alt="Logo"
+                    className="size-16"
+                />
             </div>
             {/*
                 isMobile == true && menuOpen &&
