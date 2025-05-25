0756c6f689f8571bed9853cadd8fe91f430ebdb7
Musca Mihail <muscamihaill@gmail.com>
2025-05-25 16:50:53 +0300

    added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="flex justify-center items-center h-full">
+            <h3>Pagina nu a fost gasita.</h3>
+        </div>
+    );
+}
