b4006a2c560d493db3c80001a88e4f35b235df48
Musca Mihail <muscamihaill@gmail.com>
2025-06-02 23:50:23 +0300

    remove test card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { TestCard } from "@/components/test-card";
-
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center size-full gap-2">
-            <TestCard />
         </div>
     );
 }
