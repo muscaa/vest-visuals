f977b0264bdf88b0e326f2df10be44267856f2f7
Musca Mihail <muscamihaill@gmail.com>
2025-06-03 13:03:27 +0300

    added more placeholder images, added masonry layout to portfolio, added 8xl container size

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,57 @@
+"use client";
+
+import { Masonry } from "react-plock";
+import Image from "next/image";
+
 export default function Portfolio() {
+    const placeholders = [
+        {
+            src: "/placeholder0.png",
+            width: 1200,
+            height: 800,
+            alt: "Placeholder Image 0",
+        },
+        /*{
+            src: "/placeholder1.png",
+            width: 522,
+            height: 800,
+            alt: "Placeholder Image 1",
+        },*/
+        {
+            src: "/placeholder2.png",
+            width: 1200,
+            height: 430,
+            alt: "Placeholder Image 2",
+        },
+        {
+            src: "/placeholder3.png",
+            width: 135,
+            height: 135,
+            alt: "Placeholder Image 3",
+        },
+    ];
+
+    const items = [];
+
+    for (let i = 0; i < 40; i++) {
+        const rand = Math.floor(Math.random() * placeholders.length);
+        items.push(placeholders[rand]);
+    }
+
     return (
-        <div className="flex flex-col lg:flex-row items-center justify-center size-full gap-8 py-8">
+        <div className="flex items-center justify-center size-full p-2">
+            <Masonry
+                items={items}
+                config={{
+                    columns: [1, 2, 3],
+                    gap: [8, 8, 8],
+                    media: [640, 1024, 1408],
+                }}
+                render={(item, idx) => (
+                    <Image key={idx} src={item.src} alt={item.alt} width={item.width} height={item.height} className="w-full h-auto" />
+                )}
+                className="max-w-8xl"
+            />
         </div>
     );
 }
