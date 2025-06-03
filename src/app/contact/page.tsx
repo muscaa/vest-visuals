83c49fb2ad6e38f51a162e55d3f687a53bd2ca20
Musca Mihail <muscamihaill@gmail.com>
2025-06-03 11:52:27 +0300

    make uppercase

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -119,7 +119,7 @@ export default function Contact() {
     return (
         <div className="flex flex-col lg:flex-row items-center justify-center size-full gap-8 py-8">
             <ContactForm />
-            <h4>sau</h4>
+            <h4>SAU</h4>
             <ContactOther />
         </div>
     );
