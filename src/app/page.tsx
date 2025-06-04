c1d50539bd585b8d2e0d892e07c40dc0125cd7de
Musca Mihail <muscamihaill@gmail.com>
2025-06-04 17:10:13 +0300

    added idle category auto switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,17 @@
 import { Main } from "@/components/main";
 import {
     useState,
-    //useEffect
+    useEffect
 } from "react";
 import Image from "next/image";
 
-function SectionCategories() {
-    const [category, setCategory] = useState(0);
-
-    /*useEffect(() => {
-        const interval = setInterval(() => {
-            setCategory((prev) => {
-                return (prev + 1) % 6;
-            });
-        }, 3000);
-        
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);*/
-
-    return (
-        <section className="flex not-sm:flex-col w-full h-[calc(100vh-5rem)] gap-1">
-            <Category name="1" selected={category == 0} onSelect={() => setCategory(0)} className="bg-red-400" />
-            <Category name="2" selected={category == 1} onSelect={() => setCategory(1)} className="bg-purple-400" />
-            <Category name="3" selected={category == 2} onSelect={() => setCategory(2)} className="bg-green-400" />
-            <Category name="4" selected={category == 3} onSelect={() => setCategory(3)} className="bg-blue-400" />
-            <Category name="5" selected={category == 4} onSelect={() => setCategory(4)} className="bg-yellow-400" />
-            <Category name="6" selected={category == 5} onSelect={() => setCategory(5)} className="bg-indigo-400" />
-        </section>
-    );
-}
-
-function Category(props: { name: string, selected: boolean, onSelect?: () => void, className?: string }) {
+function Category(props: { name: string, selected: boolean, hovered: boolean, onEnter?: () => void, onLeave?: () => void }) {
     return (
         <div
-            onMouseEnter={props.onSelect}
+            onMouseEnter={props.onEnter}
+            onMouseLeave={props.onLeave}
             className={`${props.selected ? "sm:w-[400%] not-sm:h-[400%]" : "sm:w-full not-sm:h-full"}
-                sm:h-full not-sm:w-full transition-all ease-out duration-700 overflow-hidden`}
+                sm:h-full not-sm:w-full transition-all ${props.hovered ? "ease-out" : "ease-in-out"} duration-700 overflow-hidden`}
         >
             <div
                 className={`${props.selected ? "" : "blur-xs saturate-0"}
@@ -48,6 +22,76 @@ function Category(props: { name: string, selected: boolean, onSelect?: () => voi
                 <Image src="/image0.png" alt="Image" width={1024} height={684} className="absolute size-full object-cover object-center" />
             </div>
         </div>
+    );
+}
+
+function SectionCategories() {
+    const [category, setCategory] = useState(0);
+    const [hovered, setHovered] = useState(false);
+
+    useEffect(() => {
+        const interval = hovered ? undefined : setInterval(() => {
+            setCategory((prev) => {
+                return (prev + 1) % 4;
+            });
+        }, 3000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [hovered]);
+
+    return (
+        <section className="flex not-sm:flex-col w-full h-[calc(100vh-5rem)] gap-1">
+            <Category
+                name="Evenimente"
+                selected={category == 0}
+                hovered={hovered}
+                onEnter={() => {
+                    setHovered(true);
+                    setCategory(0);
+                }}
+                onLeave={() => {
+                    setHovered(false);
+                }}
+            />
+            <Category
+                name="Studio"
+                selected={category == 1}
+                hovered={hovered}
+                onEnter={() => {
+                    setHovered(true);
+                    setCategory(1);
+                }}
+                onLeave={() => {
+                    setHovered(false);
+                }}
+            />
+            <Category
+                name="Automotive"
+                selected={category == 2}
+                hovered={hovered}
+                onEnter={() => {
+                    setHovered(true);
+                    setCategory(2);
+                }}
+                onLeave={() => {
+                    setHovered(false);
+                }}
+            />
+            <Category
+                name="Corporate & Commercial"
+                selected={category == 3}
+                hovered={hovered}
+                onEnter={() => {
+                    setHovered(true);
+                    setCategory(3);
+                }}
+                onLeave={() => {
+                    setHovered(false);
+                }}
+            />
+        </section>
     );
 }
 
