9a45be8992ee302834300841df7ad2f56dc1d864
Musca Mihail <muscamihaill@gmail.com>
2025-06-04 09:29:40 +0300

    create separate categories section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,33 @@ import {
 } from "react";
 import Image from "next/image";
 
+function SectionCategories() {
+    const [category, setCategory] = useState(0);
+
+    /*useEffect(() => {
+        const interval = setInterval(() => {
+            setCategory((prev) => {
+                return (prev + 1) % 6;
+            });
+        }, 3000);
+        
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);*/
+
+    return (
+        <section className="flex not-sm:flex-col w-full h-[calc(100vh-5rem)] gap-1">
+            <Category name="1" selected={category == 0} onSelect={() => setCategory(0)} className="bg-red-400" />
+            <Category name="2" selected={category == 1} onSelect={() => setCategory(1)} className="bg-purple-400" />
+            <Category name="3" selected={category == 2} onSelect={() => setCategory(2)} className="bg-green-400" />
+            <Category name="4" selected={category == 3} onSelect={() => setCategory(3)} className="bg-blue-400" />
+            <Category name="5" selected={category == 4} onSelect={() => setCategory(4)} className="bg-yellow-400" />
+            <Category name="6" selected={category == 5} onSelect={() => setCategory(5)} className="bg-indigo-400" />
+        </section>
+    );
+}
+
 function Category(props: { name: string, selected: boolean, onSelect?: () => void, className?: string }) {
     return (
         <div
@@ -25,31 +52,10 @@ function Category(props: { name: string, selected: boolean, onSelect?: () => voi
 }
 
 export default function Home() {
-    const [category, setCategory] = useState(0);
-
-    /*useEffect(() => {
-        const interval = setInterval(() => {
-            setCategory((prev) => {
-                return (prev + 1) % 6;
-            });
-        }, 3000);
-        
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);*/
-
     return (
         <Main>
             <div className="flex flex-col items-center justify-center size-full gap-2 p-2">
-                <div className="flex not-sm:flex-col w-full h-[calc(100vh-5rem)] gap-1">
-                    <Category name="1" selected={category == 0} onSelect={() => setCategory(0)} className="bg-red-400" />
-                    <Category name="2" selected={category == 1} onSelect={() => setCategory(1)} className="bg-purple-400" />
-                    <Category name="3" selected={category == 2} onSelect={() => setCategory(2)} className="bg-green-400" />
-                    <Category name="4" selected={category == 3} onSelect={() => setCategory(3)} className="bg-blue-400" />
-                    <Category name="5" selected={category == 4} onSelect={() => setCategory(4)} className="bg-yellow-400" />
-                    <Category name="6" selected={category == 5} onSelect={() => setCategory(5)} className="bg-indigo-400" />
-                </div>
+                <SectionCategories />
                 <div className="w-full h-128 bg-green-400">
 
                 </div>
