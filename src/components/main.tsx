ea8ea5c58f87888914097704bb5621aac1da1d50
Musca Mihail <muscamihaill@gmail.com>
2025-06-04 19:26:33 +0300

    added header & footer properties

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,17 +3,23 @@ import { Footer } from "@/components/footer";
 
 interface MainProps {
     children: React.ReactNode;
+    header?: React.ReactNode;
+    footer?: React.ReactNode;
 }
 
 export function Main(props: MainProps) {
     return (
         <>
-            <Navbar />
+            {
+                props.header ?? <Navbar />
+            }
             <div className="flex flex-col h-full max-h-full overflow-y-auto">
                 <main className="flex-grow">
                     {props.children}
                 </main>
-                <Footer />
+                {
+                    props.footer ?? <Footer />
+                }
             </div>
         </>
     );
