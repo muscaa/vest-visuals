aac426c45c88d33414141ba6a7ba700878debcfe
Musca Mihail <muscamihaill@gmail.com>
2025-06-22 17:48:07 +0300

    added main page reveal animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,6 +28,7 @@ import {
     type CarouselApi,
 } from "@/components/ui/carousel";
 import * as config from "@/config/public";
+import { Reveal } from "@/components/animations/reveal";
 
 function SectionMain() {
     const imageRef = useRef<HTMLImageElement>(null);
@@ -45,14 +46,16 @@ function SectionMain() {
     return (
         <section id="main" className="relative w-full h-[calc(100vh-4rem)] p-2">
             <div className="absolute size-[calc(100%-1rem)] overflow-hidden">
-                <Image
-                    ref={imageRef}
-                    src="/image0.png"
-                    alt="Image"
-                    width={1024}
-                    height={684}
-                    className="size-full object-cover object-center transition-all duration-10000 ease-linear"
-                />
+                <Reveal direction="none" className="size-full">
+                    <Image
+                        ref={imageRef}
+                        src="/image0.png"
+                        alt="Image"
+                        width={1024}
+                        height={684}
+                        className="size-full object-cover object-center transition-all duration-10000 ease-linear"
+                    />
+                </Reveal>
             </div>
             <div
                 className="
@@ -63,10 +66,16 @@ function SectionMain() {
             >
                 <div></div>
                 <div className="flex flex-col justify-center items-center gap-4">
-                    <h1 className="font-medium text-center">FOTO & VIDEO</h1>
-                    <h2 className="font-light text-center italic text-foreground3">TIMISOARA | ARAD | ORADEA</h2>
+                    <Reveal delay={500} duration={1000}>
+                        <h1 className="font-medium text-center">FOTO & VIDEO</h1>
+                    </Reveal>
+                    <Reveal delay={800} duration={1000}>
+                        <h2 className="font-light text-center italic text-foreground32">TIMISOARA | ARAD | ORADEA</h2>
+                    </Reveal>
                 </div>
-                <ButtonLink href="#about" variant="neutral" size="lg" className="theme-light">AFLA MAI MULTE</ButtonLink>
+                <Reveal delay={1800} duration={1000}>
+                    <ButtonLink href="#about" variant="neutral" size="lg" className="theme-light">AFLA MAI MULTE</ButtonLink>
+                </Reveal>
             </div>
         </section>
     );
@@ -100,60 +109,68 @@ function SectionAbout() {
 
     return (
         <section id="about" className="flex flex-col justify-center items-center gap-4 p-2">
-            <h1>Cine suntem noi?</h1>
+            <Reveal delay={200}>
+                <h1>Cine suntem noi?</h1>
+            </Reveal>
             <div className="flex flex-col justify-center items-center gap-2">
-                <h4>multe</h4>
+                <Reveal delay={400}>
+                    <h4>oameni</h4>
+                </Reveal>
             </div>
-            <h1>Cu ce ne ocupam?</h1>
-            <Carousel 
-                setApi={setApi}
-                opts={{
-                    loop: true,
-                }}
-                className="w-full"
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
-            >
-                <CarouselContent>
-                    {
-                        config.categories.map((category, index) => (
-                            <CarouselItem key={index} className="md:basis-3/5">
-                                <div
-                                    onClick={() => api?.scrollTo(index)}
-                                    className="relative w-full h-112"
-                                >
-                                    <Image
-                                        src={category.coverImage.src}
-                                        alt={category.coverImage.alt}
-                                        width={category.coverImage.w}
-                                        height={category.coverImage.h}
-                                        className={`
-                                            absolute size-full object-cover object-center
-                                            transition-all duration-500
-                                            ${index != current - 1 ? "md:saturate-0 md:opacity-75 md:contrast-50 md:scale-95" : ""}
-                                        `}
-                                    />
+            <Reveal delay={600}>
+                <h1>Cu ce ne ocupam?</h1>
+            </Reveal>
+            <Reveal delay={800} duration={1000} direction="none" className="w-full">
+                <Carousel 
+                    setApi={setApi}
+                    opts={{
+                        loop: true,
+                    }}
+                    className="w-full"
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                >
+                    <CarouselContent>
+                        {
+                            config.categories.map((category, index) => (
+                                <CarouselItem key={index} className="md:basis-3/5">
                                     <div
-                                        className={`
-                                            absolute flex not-md:flex-col size-full
-                                            justify-end items-center md:justify-between md:items-end
-                                            p-4 gap-2 theme-dark bg-gradient-to-b from-transparent to-black/30
-                                            transition-all duration-500 ${index != current - 1 ? "md:opacity-0" : "md:opacity-100"}
-                                        `}
+                                        onClick={() => api?.scrollTo(index)}
+                                        className="relative w-full h-112"
                                     >
-                                        <h2 className="not-md:text-center">{category.name.toUpperCase()}</h2>
-                                        <ButtonLink href={category.portfolioUrl} variant="neutral" className="theme-light">
-                                            PORTOFOLIU
-                                        </ButtonLink>
+                                        <Image
+                                            src={category.coverImage.src}
+                                            alt={category.coverImage.alt}
+                                            width={category.coverImage.w}
+                                            height={category.coverImage.h}
+                                            className={`
+                                                absolute size-full object-cover object-center
+                                                transition-all duration-500
+                                                ${index != current - 1 ? "md:saturate-0 md:opacity-75 md:contrast-50 md:scale-95" : ""}
+                                            `}
+                                        />
+                                        <div
+                                            className={`
+                                                absolute flex not-md:flex-col size-full
+                                                justify-end items-center md:justify-between md:items-end
+                                                p-4 gap-2 theme-dark bg-gradient-to-b from-transparent to-black/30
+                                                transition-all duration-500 ${index != current - 1 ? "md:opacity-0" : "md:opacity-100"}
+                                            `}
+                                        >
+                                            <h2 className="not-md:text-center">{category.name.toUpperCase()}</h2>
+                                            <ButtonLink href={category.portfolioUrl} variant="neutral" className="theme-light">
+                                                PORTOFOLIU
+                                            </ButtonLink>
+                                        </div>
                                     </div>
-                                </div>
-                            </CarouselItem>
-                        ))
-                    }
-                </CarouselContent>
-                <CarouselPrevious variant="transparent" className="left-2" />
-                <CarouselNext variant="transparent" className="right-2" />
-            </Carousel>
+                                </CarouselItem>
+                            ))
+                        }
+                    </CarouselContent>
+                    <CarouselPrevious variant="transparent" className="left-2" />
+                    <CarouselNext variant="transparent" className="right-2" />
+                </Carousel>
+            </Reveal>
         </section>
     );
 }
@@ -161,33 +178,41 @@ function SectionAbout() {
 function Member(props: { name: string, roles: string[], socials: { [key: string]: Icon } }) {
     return (
         <div className="flex flex-col w-64 md:w-96 xl:w-128 p-8 justify-center items-center gap-4">
-            <div className="size-64 md:size-96 xl:size-128 bg-blue-400">
+            <Reveal duration={1000}>
+                <div className="size-64 md:size-96 xl:size-128 bg-blue-400">
 
-            </div>
+                </div>
+            </Reveal>
             <div className="flex flex-col justify-center items-center gap-1">
-                <h2>{props.name}</h2>
-                <div className="flex flex-wrap justify-center items-center gap-2">
+                <Reveal delay={300}>
+                    <h2>{props.name}</h2>
+                </Reveal>
+                <Reveal delay={500}>
+                    <div className="flex flex-wrap justify-center items-center gap-2">
+                        {
+                            props.roles.map((role, index) => (
+                                <div key={index} className="flex h-6 items-center gap-2">
+                                    <h4 className="text-muted-foreground">{role}</h4>
+                                    {
+                                        index < props.roles.length - 1 && (
+                                            <Separator orientation="vertical" />
+                                        )
+                                    }
+                                </div>
+                            ))
+                        }
+                    </div>
+                </Reveal>
+            </div>
+            <Reveal delay={700}>
+                <div className="flex items-center justify-center gap-4">
                     {
-                        props.roles.map((role, index) => (
-                            <div key={index} className="flex h-6 items-center gap-2">
-                                <h4 className="text-muted-foreground">{role}</h4>
-                                {
-                                    index < props.roles.length - 1 && (
-                                        <Separator orientation="vertical" />
-                                    )
-                                }
-                            </div>
+                        Object.entries(props.socials).map(([key, icon], index) => (
+                            <IconLink key={index} href={key} icon={icon} />
                         ))
                     }
                 </div>
-            </div>
-            <div className="flex items-center justify-center gap-4">
-                {
-                    Object.entries(props.socials).map(([key, icon], index) => (
-                        <IconLink key={index} href={key} icon={icon} />
-                    ))
-                }
-            </div>
+            </Reveal>
         </div>
     );
 }
@@ -195,7 +220,9 @@ function Member(props: { name: string, roles: string[], socials: { [key: string]
 function SectionTeam() {
     return (
         <section id="team" className="flex flex-col justify-center items-center px-2 py-8">
-            <h1>Cunoaste echipa</h1>
+            <Reveal delay={200}>
+                <h1>Cunoaste echipa</h1>
+            </Reveal>
             <div className="flex flex-wrap w-full max-w-8xl justify-evenly gap-x-8">
                 <Member
                     name="David"
