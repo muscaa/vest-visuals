9cde33195020e7eaaae81202f6008928d683cca0
Musca Mihail <muscamihaill@gmail.com>
2025-06-24 17:01:53 +0300

    added warning to unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.config({
+        extends: [
+            "next/core-web-vitals",
+            "next/typescript"
+        ],
+        rules: {
+            "no-unused-vars": "warn",
+            "@typescript-eslint/no-unused-vars": "warn"
+        }
+    }),
 ];
 
 export default eslintConfig;
