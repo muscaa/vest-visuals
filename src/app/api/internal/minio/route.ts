71fbc1afe1cf80320347158212276204d89041fe
Musca Mihail <muscamihaill@gmail.com>
2025-06-26 18:44:07 +0300

    added host header

--- src/app/api/internal/minio/route.ts
+++ src/app/api/internal/minio/route.ts
@@ -3,10 +3,14 @@ import {
     NextResponse
 } from "next/server";
 import * as config from "@/config/server";
+import { headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
+    const headersList = await headers();
+
     return NextResponse.json({
         url: config.env.S3_CONSOLE_URL,
         request_url: request.url,
+        host: headersList.get("host"),
     });
 }
