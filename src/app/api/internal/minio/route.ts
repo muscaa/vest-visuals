c817e8f261708333d0fe198a4d822a2bf2515f5b
Musca Mihail <muscamihaill@gmail.com>
2025-06-26 19:40:06 +0300

    remove unused output, add real url output

--- src/app/api/internal/minio/route.ts
+++ src/app/api/internal/minio/route.ts
@@ -7,11 +7,8 @@ import { headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
     const headersList = await headers();
-    const upgrade = headersList.get("upgrade");
-    const connection = headersList.get("connection");
     const host = headersList.get("host");
-    const xRealIp = headersList.get("x-real-ip");
-    const xForwardedFor = headersList.get("x-forwarded-for");
+    const xRealUrl = headersList.get("x-real-url");
 
     const url = new URL(request.url);
     if (host != null) {
@@ -24,10 +21,6 @@ export async function GET(request: NextRequest) {
         request_url: request.url,
         host_url: url.toJSON(),
         host_url_string: url.toString(),
-        upgrade,
-        connection,
-        host,
-        xRealIp,
-        xForwardedFor,
+        x_real_url: xRealUrl,
     });
 }
