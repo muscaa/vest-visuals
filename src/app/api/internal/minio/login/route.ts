d8a4da21887310a297c6ec8559c715b5e569aa4c
Musca Mihail <muscamihaill@gmail.com>
2025-06-26 21:07:00 +0300

    fix redirects

--- src/app/api/internal/minio/login/route.ts
+++ src/app/api/internal/minio/login/route.ts
@@ -4,21 +4,20 @@ import {
 } from "next/server";
 import * as config from "@/config/server";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 export async function GET(request: NextRequest) {
     const headersList = await headers();
     const xRealUrl = headersList.get("x-real-url");
 
     if (xRealUrl == null || !xRealUrl.startsWith(config.env.S3_CONSOLE_URL)) {
-        return redirect(config.env.S3_CONSOLE_URL);
+        return NextResponse.redirect(config.env.S3_CONSOLE_URL);
     }
 
     const url = new URL(xRealUrl);
     const token = url.searchParams.get("token");
 
     if (token == null) {
-        return redirect(config.env.S3_CONSOLE_URL);
+        return NextResponse.redirect(config.env.S3_CONSOLE_URL);
     }
 
     const response = NextResponse.redirect(config.env.S3_CONSOLE_URL);
