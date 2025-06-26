64ce4e2e6bd14b062909017b26392205db77d136
Musca Mihail <muscamihaill@gmail.com>
2025-06-26 17:41:51 +0300

    added portfolio layout, added /internal route with root layout

---
+++ src/app/internal/layout.tsx
@@ -0,0 +1,11 @@
+import {
+    RootLayout,
+    createMetadata,
+} from "@/components/layout";
+
+export const metadata = createMetadata({
+    route: "/",
+    routeName: "Internal",
+});
+
+export default RootLayout;
