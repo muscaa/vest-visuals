25416e5ebf7bc706fc38a9acb9992fd39b5aa3b6
Musca Mihail <muscamihaill@gmail.com>
2025-06-26 18:34:38 +0300

    refactor

--- src/app/api/internal/minio/route.ts
+++ src/app/api/internal/minio/route.ts
@@ -7,6 +7,6 @@ import * as config from "@/config/server";
 export async function GET(request: NextRequest) {
     return NextResponse.json({
         url: config.env.S3_CONSOLE_URL,
-        request
+        request_url: request.url,
     });
 }
