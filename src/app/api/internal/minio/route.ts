b2a5a68048ac9a08516a275554957298b959cfde
Musca Mihail <muscamihaill@gmail.com>
2025-06-26 19:25:02 +0300

    added more output

--- src/app/api/internal/minio/route.ts
+++ src/app/api/internal/minio/route.ts
@@ -7,7 +7,11 @@ import { headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
     const headersList = await headers();
+    const upgrade = headersList.get("upgrade");
+    const connection = headersList.get("connection");
     const host = headersList.get("host");
+    const xRealIp = headersList.get("x-real-ip");
+    const xForwardedFor = headersList.get("x-forwarded-for");
 
     const url = new URL(request.url);
     if (host != null) {
@@ -18,8 +22,12 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({
         url: config.env.S3_CONSOLE_URL,
         request_url: request.url,
-        host: host,
         host_url: url.toJSON(),
         host_url_string: url.toString(),
+        upgrade,
+        connection,
+        host,
+        xRealIp,
+        xForwardedFor,
     });
 }
