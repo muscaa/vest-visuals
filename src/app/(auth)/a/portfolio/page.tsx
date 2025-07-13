941c5db9f18f659c2a653cacdfe64a5a60bae52a
Musca Mihail <muscamihaill@gmail.com>
2025-07-13 14:29:59 +0300

    added portfolio category admin page

--- src/app/(auth)/a/portfolio/page.tsx
+++ src/app/(auth)/a/portfolio/page.tsx
@@ -19,6 +19,7 @@ export default function Page() {
                             >
                                 <CategoryLink
                                     category={category}
+                                    href={`/a${category.portfolioUrl}`}
                                 />
                             </Reveal>
                         ))
