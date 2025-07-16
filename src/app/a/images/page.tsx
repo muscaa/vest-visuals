4b38e732d617cd82063cd7e649b1f0d07be5e56a
Musca Mihail <muscamihaill@gmail.com>
2025-07-17 00:46:42 +0300

    fix dialog close button size, added example dialog to admin images

--- src/app/a/images/page.tsx
+++ src/app/a/images/page.tsx
@@ -5,6 +5,19 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import * as types from "@/types/api/images";
 import { ImagesItem } from "@/types/db/images";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface ImagesItemEntryProps {
     item: ImagesItem;
@@ -49,8 +62,39 @@ export default function Page() {
 
     return (
         <MainAdmin>
-            <div className="flex justify-center items-center size-full bg-red-400">
-
+            <div className="flex justify-center items-center size-full">
+                <Dialog>
+                    <form>
+                        <DialogTrigger asChild>
+                            <Button variant="outline">Open Dialog</Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>Edit profile</DialogTitle>
+                                <DialogDescription>
+                                    Make changes to your profile here. Click save when you&apos;re
+                                    done.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div className="grid gap-4">
+                                <div className="grid gap-3">
+                                    <Label htmlFor="name-1">Name</Label>
+                                    <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+                                </div>
+                                <div className="grid gap-3">
+                                    <Label htmlFor="username-1">Username</Label>
+                                    <Input id="username-1" name="username" defaultValue="@peduarte" />
+                                </div>
+                            </div>
+                            <DialogFooter>
+                                <DialogClose asChild>
+                                    <Button variant="outline">Cancel</Button>
+                                </DialogClose>
+                                <Button type="submit">Save changes</Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </form>
+                </Dialog>
 
                 {/*<div className="flex flex-col max-w-md w-full gap-2 p-2">
                     {
