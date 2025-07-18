52a9c5a740446205cab8d33d350de6f2dc69f317
Musca Mihail <muscamihaill@gmail.com>
2025-07-18 19:07:42 +0300

    added open button, add flex wrap to entry

--- src/app/a/images/page.tsx
+++ src/app/a/images/page.tsx
@@ -27,7 +27,7 @@ function ImagesRecordEntry(props: ImagesRecordEntryProps) {
             onClick={() => props.onSelect(props.record)}
             className={props.selected ? "border-accent-foreground dark:border-accent-foreground" : ""}
         >
-            <div className="flex gap-4 size-full whitespace-normal">
+            <div className="flex flex-wrap gap-4 size-full whitespace-normal">
                 <Image src="/placeholder0.png" alt="placeholder" width={128} height={128} className="w-32 object-contain" />
                 <div className="flex flex-col gap-1 grow">
                     <h4>{props.record.group}</h4>
@@ -82,7 +82,7 @@ export default function Page() {
         <MainAdmin extraClassName="overflow-hidden">
             <div className="flex justify-center items-center size-full p-2">
                 <div className="flex flex-col size-full max-w-4xl max-h-144 gap-2">
-                    <div className="flex gap-2">
+                    <div className="flex flex-wrap gap-2">
                         <ImagesCreateDialog
                             onCreate={handleUpdate}
                         >
@@ -92,6 +92,13 @@ export default function Page() {
                                 New
                             </Button>
                         </ImagesCreateDialog>
+                        <Button
+                            variant="secondary"
+                            disabled={!selectedRecord}
+                            className="grow"
+                        >
+                            Open
+                        </Button>
                         <ImagesEditDialog
                             record={selectedRecord}
                             onEdit={handleUpdate}
