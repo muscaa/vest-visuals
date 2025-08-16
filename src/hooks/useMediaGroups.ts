4820b2c0e4a0829dffc0652b96c72827cb9c67b9
Musca Mihail <muscamihaill@gmail.com>
2025-08-16 14:25:03 +0300

    feat: added create, update & remove media groups hook functions

--- src/hooks/useMediaGroups.ts
+++ src/hooks/useMediaGroups.ts
@@ -5,6 +5,9 @@ import {
     useMutation,
 } from "@tanstack/react-query";
 import { api_routes } from "@/utils/client/axios";
+import * as types_create from "@/types/api/media/groups/create";
+import * as types_update from "@/types/api/media/groups/update";
+import * as types_remove from "@/types/api/media/groups/remove";
 
 export function useMediaCategories() {
     const getMediaGroups = () => useQuery({
@@ -30,4 +33,46 @@ export function useMediaCategories() {
             return data.value || [];
         },
     });
+
+    const createMediaGroup = useMutation({
+        mutationKey: [api_routes.media.groups.create._.url],
+        mutationFn: async (props: types_create.PostRequest) => {
+            const { data } = await api_routes.media.groups.create._.post(props);
+
+            if (!data.success) throw new Error("Failed to create media group");
+            if (!data.value) throw new Error("No media group returned");
+
+            return data.value;
+        },
+    });
+
+    const updateMediaGroup = useMutation({
+        mutationKey: [api_routes.media.groups.update._.url],
+        mutationFn: async (props: types_update.PostRequest) => {
+            const { data } = await api_routes.media.groups.update._.post(props);
+
+            if (!data.success) throw new Error("Failed to update media group");
+
+            return data.success;
+        },
+    });
+
+    const removeMediaGroup = useMutation({
+        mutationKey: [api_routes.media.groups.remove._.url],
+        mutationFn: async (props: types_remove.PostRequest) => {
+            const { data } = await api_routes.media.groups.remove._.post(props);
+
+            if (!data.success) throw new Error("Failed to remove media group");
+
+            return data.success;
+        },
+    });
+
+    return {
+        getMediaGroups,
+        getMediaGroup,
+        createMediaGroup,
+        updateMediaGroup,
+        removeMediaGroup,
+    };
 }
