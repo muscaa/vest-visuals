6311814e73b265bcdaafafbf4978ba03da4052ae
Musca Mihail <muscamihaill@gmail.com>
2025-08-16 16:28:08 +0300

    refactor: rewrite admin media groups page

--- src/app/a/media/groups/page.tsx
+++ src/app/a/media/groups/page.tsx
@@ -1,172 +1,122 @@
 "use client";
 
 import { MainAdmin } from "@/components/admin/main";
-import { useQuery } from "@tanstack/react-query";
-import { useMemo, useState } from "react";
-import * as types from "@/types/api/media/groups";
-import { MediaGroupsRecord } from "@/types/db/mediaGroups";
-import { api_client } from "@/utils/client/axios";
+import {
+    useMemo,
+    useState,
+} from "react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { useRouter } from "next/navigation";
-import { MediaGroupsCreateDialog } from "@/components/dialogs/media-groups-create";
-import { MediaGroupsEditDialog } from "@/components/dialogs/media-groups-edit";
-import { MediaGroupsDeleteDialog } from "@/components/dialogs/media-groups-delete";
 import { Img } from "@/components/snippets";
 import { myDate } from "@/utils/snippets";
+import { List } from "@/components/list";
+import { useMediaGroups } from "@/hooks/useMediaGroups";
+import { Value } from "@/types/api/media/groups";
+import { MediaGroupsCreateDialog } from "@/components/dialogs/media-groups-create";
+import { MediaGroupsDeleteDialog } from "@/components/dialogs/media-groups-delete";
 
-interface MediaGroupsRecordEntryProps {
-    record: MediaGroupsRecord;
-    selected: boolean;
-    onSelect: (record: MediaGroupsRecord) => void;
+interface ListEntryProps {
+    value: Value;
 }
 
-function MediaGroupsRecordEntry(props: MediaGroupsRecordEntryProps) {
+export function MediaGroupsListEntry(props: ListEntryProps) {
     const image = useMemo(() => {
-        if (props.record.expand && props.record.expand.mediaVariants && props.record.expand.mediaVariants.length > 0) {
-            const mediaVariant = props.record.expand.mediaVariants[0];
+        /*if (props.value.expand && props.value.expand.mediaVariants && props.value.expand.mediaVariants.length > 0) {
+            const mediaVariant = props.value.expand.mediaVariants[0];
 
             if (mediaVariant.expand && mediaVariant.expand.media && mediaVariant.expand.media.length > 0) {
                 const media = mediaVariant.expand.media[0];
-                
+
                 return media.file;
             }
-        }
+        }*/
 
         return "/placeholder0.png";
-    }, [props.record]);
+    }, [props.value]);
 
     return (
-        <Button
-            variant="card"
-            size="none"
-            onClick={() => props.onSelect(props.record)}
-            className={props.selected ? "border-accent-foreground dark:border-accent-foreground" : ""}
-        >
-            <div className="flex flex-wrap gap-4 size-full whitespace-normal">
-                <Img
-                    src={image}
-                    alt="Preview"
-                    width={128}
-                    height={128}
-                    className="size-32 object-contain"
-                />
-                <div className="flex flex-col gap-1 grow">
-                    <h4>{props.record.id}</h4>
-                    <Separator />
-                    <div className="flex gap-2 text-muted-foreground">
-                        <div className="flex flex-col grow">
-                            <p>{props.record.category}</p>
-                            <h6>Updated: {myDate(props.record.updated)}</h6>
-                            <h6>Created: {myDate(props.record.created)}</h6>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                            <p>{props.record.mediaVariants?.length || "no"}</p>
-                            <h5>items</h5>
-                        </div>
+        <div className="flex flex-wrap gap-4 size-full whitespace-normal">
+            <Img
+                src={image}
+                alt="Preview"
+                width={128}
+                height={128}
+                className="size-32 object-contain"
+            />
+            <div className="flex flex-col gap-1 grow">
+                <h4>{props.value.id}</h4>
+                <Separator />
+                <div className="flex gap-2 text-muted-foreground">
+                    <div className="flex flex-col grow">
+                        <p>{props.value.id}</p>
+                        <h6>Updated: {myDate(props.value.updated)}</h6>
+                        <h6>Created: {myDate(props.value.created)}</h6>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <p>{props.value.media?.length || "no"}</p>
+                        <h5>items</h5>
                     </div>
                 </div>
             </div>
-        </Button>
+        </div>
     );
 }
 
 export default function Page() {
     const router = useRouter();
-    const [filter, setFilter] = useState<string>();
-    const [sort, setSort] = useState<string>();
-    const [selectedRecord, setSelectedRecord] = useState<MediaGroupsRecord>();
+    const [selected, setSelected] = useState<Value>();
+    const { getMediaGroups } = useMediaGroups();
+    const { data, refetch } = getMediaGroups();
 
-    const { data, refetch } = useQuery({
-        queryKey: ["media/groups", filter, sort],
-        queryFn: async () => {
-            const { data } = await api_client.post<types.PostResponse, types.PostRequest>("/media/groups", {
-                filter,
-                sort,
-            });
-
-            if (!data.success) return [];
-
-            return data.values || [];
-        },
-    });
-
-    const handleSelect = (record: MediaGroupsRecord) => {
-        setSelectedRecord(selectedRecord?.id == record.id ? undefined : record);
+    const handleSelect = (value: Value) => {
+        setSelected(selected?.id == value.id ? undefined : value);
     };
 
     const handleUpdate = () => {
-        setSelectedRecord(undefined);
-        
+        setSelected(undefined);
+
         refetch();
     };
 
     return (
         <MainAdmin extraClassName="overflow-hidden">
-            <div className="flex justify-center items-center size-full p-2">
-                <div className="flex flex-col size-full max-w-4xl max-h-144 gap-2">
-                    <div className="flex flex-wrap gap-2">
-                        <MediaGroupsCreateDialog
-                            onCreate={handleUpdate}
-                        >
-                            <Button
-                                className="grow"
-                            >
-                                New
-                            </Button>
-                        </MediaGroupsCreateDialog>
-                        <Button
-                            variant="secondary"
-                            disabled={!selectedRecord}
-                            onClick={() => router.push(`/a/media/groups/${selectedRecord?.id}`)}
-                            className="grow"
-                        >
-                            Open
-                        </Button>
-                        <MediaGroupsEditDialog
-                            record={selectedRecord}
-                            onEdit={handleUpdate}
-                        >
-                            <Button
-                                variant="secondary"
-                                disabled={!selectedRecord}
-                                className="grow"
-                            >
-                                Edit
-                            </Button>
-                        </MediaGroupsEditDialog>
-                        <MediaGroupsDeleteDialog
-                            record={selectedRecord}
-                            onDelete={handleUpdate}
-                        >
-                            <Button
-                                variant="secondary"
-                                disabled={!selectedRecord}
-                                className="grow"
-                            >
-                                Delete
-                            </Button>
-                        </MediaGroupsDeleteDialog>
-                    </div>
-                    <Separator />
-                    <div className="flex flex-col max-h-full h-full overflow-y-auto">
-                        <div className="flex flex-col grow gap-2">
-                            {
-                                data && (
-                                    data.map((item, index) => (
-                                        <MediaGroupsRecordEntry
-                                            key={index}
-                                            record={item}
-                                            selected={selectedRecord?.id == item.id}
-                                            onSelect={handleSelect}
-                                        />
-                                    ))
-                                )
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <List
+                data={data}
+                isSelected={(value) => selected?.id == value.id}
+                onSelect={handleSelect}
+                entry={(value) => <MediaGroupsListEntry value={value} />}
+            >
+                <MediaGroupsCreateDialog
+                    onCreate={handleUpdate}
+                >
+                    <Button
+                        className="grow"
+                    >
+                        New
+                    </Button>
+                </MediaGroupsCreateDialog>
+                <Button
+                    variant="secondary"
+                    disabled={!selected}
+                    onClick={() => router.push(`/a/media/groups/${selected?.id}`)}
+                    className="grow"
+                >
+                    Open
+                </Button>
+                <MediaGroupsDeleteDialog
+                    value={selected}
+                    onDelete={handleUpdate}
+                >
+                    <Button
+                        variant="secondary"
+                        disabled={!selected}
+                        className="grow"
+                    >
+                        Delete
+                    </Button>
+                </MediaGroupsDeleteDialog>
+            </List>
         </MainAdmin>
     );
 }
