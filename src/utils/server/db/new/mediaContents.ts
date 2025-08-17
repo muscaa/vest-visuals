d9ea2e51107ed57ce730a6bcc8f7b2304012773d
Musca Mihail <muscamihaill@gmail.com>
2025-08-17 13:35:47 +0300

    feat: added media contents db get functions

--- src/utils/server/db/new/mediaContents.ts
+++ src/utils/server/db/new/mediaContents.ts
@@ -1,5 +1,9 @@
 import PocketBase from "pocketbase";
 import {
+    RecordOptions,
+    RecordFullListOptions,
+} from "pocketbase";
+import {
     createClientDB,
     newMediaVariantsDB,
 } from "@/utils/server/db";
@@ -27,6 +31,41 @@ export function format(record: Record) {
     }
 }
 
+interface GetProps {
+    pb?: PocketBase;
+    id: string;
+    options?: RecordOptions;
+}
+
+export async function get(props: GetProps) {
+    props.pb ||= await createClientDB();
+
+    try {
+        const result = await props.pb.collection(COLLECTION_NAME).getOne<Record>(props.id, props.options);
+        format(result);
+        return result;
+    } catch (error) {}
+
+    return null;
+}
+
+interface GetListProps {
+    pb?: PocketBase;
+    options?: RecordFullListOptions;
+}
+
+export async function getList(props: GetListProps) {
+    props.pb ||= await createClientDB();
+
+    try {
+        const result = await props.pb.collection(COLLECTION_NAME).getFullList<Record>(props.options);
+        result.forEach(format);
+        return result;
+    } catch (error) {}
+
+    return null;
+}
+
 interface CreateProps {
     pb?: PocketBase;
     value: Value;
@@ -41,9 +80,11 @@ export async function create(props: CreateProps) {
             value,
         })));
 
-        return await props.pb.collection(COLLECTION_NAME).create<Record>({
+        const result = await props.pb.collection(COLLECTION_NAME).create<Record>({
             mediaVariants: results.flatMap((mediaVariant) => mediaVariant ? [mediaVariant.id] : []),
         });
+        format(result);
+        return result;
     } catch (error) {}
 
     return null;
