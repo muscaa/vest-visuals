844009d6c96bfc85a40c41988dc8728ba43ad012
Musca Mihail <muscamihaill@gmail.com>
2025-08-25 21:11:00 +0300

    refactor: added db auth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
     out: "./.data/drizzle",
     schema: "./src/lib/server/db/schema",
-    dialect: "sqlite",
+    dialect: "turso",
     dbCredentials: {
-        url: process.env.DB_URL!,
+        url: process.env.LIBSQL_URL!,
+        authToken: process.env.LIBSQL_AUTH!,
     },
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,5 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
-const client = createClient({ url: process.env.DB_URL! });
+const client = createClient({
+    url: process.env.LIBSQL_URL!,
+    authToken: process.env.LIBSQL_AUTH!,
+});
 export const db = drizzle({ client });
