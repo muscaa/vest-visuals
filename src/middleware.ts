63dcdeece5d546edc59213f7284bf2f8d5abf496
Musca Mihail <muscamihaill@gmail.com>
2025-08-26 17:36:20 +0300

    refactor(middleware): fix imports

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import {
     NextRequest,
     NextResponse,
 } from "next/server";
-import { getUrlString } from "@/utils/server/request";
+import { getUrlString } from "@server/http";
 import { auth } from "@server/auth";
 
 export async function middleware(request: NextRequest) {
