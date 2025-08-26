b563f84a622f988761be48b85d61773a4b4a6ac9
Musca Mihail <muscamihaill@gmail.com>
2025-08-26 17:30:38 +0300

    refactor: added server http lib

---
+++ src/lib/server/http.ts
@@ -0,0 +1,43 @@
+import {
+    NextRequest,
+    NextResponse,
+} from "next/server";
+
+export async function safeJSON<T>(input: NextRequest | string, validate?: (json: T) => any) {
+    try {
+        let json: T;
+        switch (typeof input) {
+            case "string":
+                json = JSON.parse(input);
+                break;
+            default:
+                json = await input.json();
+                break;
+        }
+
+        if (validate && !validate(json)) {
+            return null;
+        }
+        
+        return json;
+    } catch (error) {}
+    return null;
+}
+
+export function getUrl(request: NextRequest, path?: string) {
+    const xRealUrl = request.headers.get("x-real-url");
+    const base = xRealUrl ?? request.url;
+    const url = new URL(path ?? base, base);
+    return url;
+}
+
+export function getUrlString(request: NextRequest, path?: string) {
+    const url = getUrl(request, path);
+    return url.toString();
+}
+
+export function responseJSON<T>(status: number, body: T) {
+    return NextResponse.json<T>(body, {
+        status,
+    });
+}
