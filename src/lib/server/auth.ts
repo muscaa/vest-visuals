6d852b5df094bde6dd9c6e74731643ebfaf64601
Musca Mihail <muscamihaill@gmail.com>
2025-08-26 16:36:30 +0300

    refactor: disable sign-ups

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,6 +3,10 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@server/db";
 import * as schema from "@server/db/schema/auth";
 import { openAPI } from "better-auth/plugins";
+import {
+    createAuthMiddleware,
+    APIError,
+} from "better-auth/api";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -16,4 +20,13 @@ export const auth = betterAuth({
     plugins: [
         openAPI(),
     ],
+    hooks: {
+        before: createAuthMiddleware(async (ctx) => {
+            if (ctx.path.startsWith("/sign-up")) {
+                throw new APIError("UNAUTHORIZED", {
+                    message: "Sign-ups are disabled",
+                });
+            }
+        }),
+    },
 });
