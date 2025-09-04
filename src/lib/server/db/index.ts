0dd48134accbb5963afa5e5d72395c8a116fddbc
Musca Mihail <muscamihaill@gmail.com>
2025-09-04 21:10:43 +0300

    refactor: replace process.env with serverConfig.env

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
+import { serverConfig } from "@server/config";
 
 export const libsql = createClient({
-    url: process.env.LIBSQL_URL!,
-    authToken: process.env.LIBSQL_AUTH!,
+    url: serverConfig.env.LIBSQL_URL,
+    authToken: serverConfig.env.LIBSQL_AUTH,
 });
 export const db = drizzle({ client: libsql });
