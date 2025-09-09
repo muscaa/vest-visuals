d124bb1796634a5bfeb1e0babb6338c1b3a32169
Musca Mihail <muscamihaill@gmail.com>
2025-09-10 00:39:43 +0300

    refactor: fix mail cli command

--- src/lib/server/cli/commands/mail.ts
+++ src/lib/server/cli/commands/mail.ts
@@ -4,8 +4,17 @@ import { sendMail } from "@server/mail";
 export const mail: CliCommand = (program, core) => {
     program
         .command("mail")
-        .action(async () => {
-            const info = await sendMail(["example@gmail.com"], "something", "something");
+        .option("--to <string>", "destination email")
+        .option("-s, --subject <string>", "email title", "no subject")
+        .argument("<string>")
+        .action(async (body: string, options) => {
+            const to: string = options.to;
+            const subject: string = options.subject;
+
+            if (!to) throw new Error("destination email is required");
+            if (!subject) throw new Error("email title is required");
+
+            const info = await sendMail([to], subject, body);
             core.out(info);
         });
 };
