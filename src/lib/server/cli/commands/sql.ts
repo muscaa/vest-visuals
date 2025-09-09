74e1043d198615a64a313ce1325af6b756f47a31
Musca Mihail <muscamihaill@gmail.com>
2025-09-10 00:30:19 +0300

    feat: added shortcuts to sql cli command

--- src/lib/server/cli/commands/sql.ts
+++ src/lib/server/cli/commands/sql.ts
@@ -1,12 +1,22 @@
 import { CliCommand } from "../core";
 import { libsql } from "@server/db";
 
+const shortcuts: Record<string, string> = {
+    tables: "SELECT name FROM sqlite_master WHERE type='table';",
+};
+
 export const sql: CliCommand = (program, core) => {
     program
         .command("sql")
         .argument("<string>")
-        .action(async (command: string) => {
+        .option("-s, --shortcut")
+        .action(async (command: string, options) => {
             try {
+                if (options.shortcut) {
+                    command = shortcuts[command];
+                    if (!command) throw new Error("Unknown shortcut");
+                }
+
                 const result = await libsql.execute(command);
                 core.out("result: ", result.toJSON());
             } catch (error) {
