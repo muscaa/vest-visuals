c06803d7e62713b83909118a9e0deaa683637274
Musca Mihail <muscamihaill@gmail.com>
2025-09-10 13:23:58 +0300

    feat: added email otp client to auth client

--- src/lib/client/auth.ts
+++ src/lib/client/auth.ts
@@ -1,4 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+import {
+    emailOTPClient,
+} from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
+    plugins: [
+        emailOTPClient(),
+    ],
 });
