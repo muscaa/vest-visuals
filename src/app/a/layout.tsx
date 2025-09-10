04285fd8d00c0099824c28aa5abd8ac0f6c41dcb
Musca Mihail <muscamihaill@gmail.com>
2025-09-10 10:49:14 +0300

    fix: added auth guard in admin layout

--- src/app/a/layout.tsx
+++ src/app/a/layout.tsx
@@ -1,11 +1,27 @@
 import {
     BaseLayout,
+    LayoutProps,
     createMetadata,
 } from "@/components/layout";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { auth } from "@server/auth";
 
 export const metadata = createMetadata({
     route: "/a",
     routeName: "Admin",
 });
 
-export default BaseLayout;
+export default async function Layout(props: LayoutProps) {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    
+    if (!session) {
+        redirect("/login");
+    }
+
+    return (
+        <BaseLayout {...props} />
+    );
+}
