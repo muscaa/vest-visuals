d53654819a20870faa7d9753ace2339bbd82685f
Musca Mihail <muscamihaill@gmail.com>
2025-09-19 22:04:53 +0300

    feat: added emails for new sign ins

--- src/lib/server/auth/hooks.ts
+++ src/lib/server/auth/hooks.ts
@@ -6,6 +6,7 @@ import {
 import { APIError } from "better-auth/api";
 import { Status } from "@type/http";
 import * as templates from "@server/mail/templates";
+import { sendMail } from "@server/mail";
 
 type Hook = Omit<MiddlewareContext<MiddlewareOptions>, "context">;
 type BeforeContext = AuthContext;
@@ -32,8 +33,10 @@ export async function beforeHook(hook: Hook, ctx: BeforeContext) {
 
 export async function afterHook(hook: Hook, ctx: AfterContext) {
     if (hook.path == "/sign-in/email") {
-        if (ctx.returned.statusCode == 200) {
-            console.log(templates.newSignIn());
+        if (!ctx.returned.body) { // only statusCode != 200 have a body??
+            const email: string = hook.body.email;
+
+            sendMail(email, templates.newSignIn());
         }
     }
 }
