8acc57beb8bdcef9a0edb5b68f173d7514fba276
Musca Mihail <muscamihaill@gmail.com>
2025-09-22 01:28:49 +0300

    feat: added auth types

---
+++ src/lib/type/auth.ts
@@ -0,0 +1,14 @@
+export interface Profile {
+    id: string;
+    email: string;
+    emailVerified: boolean;
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+    image?: string | null | undefined;
+    twoFactorEnabled: boolean | null | undefined;
+    banned: boolean | null | undefined;
+    role?: string | null | undefined;
+    banReason?: string | null | undefined;
+    banExpires?: Date | null | undefined;
+}
