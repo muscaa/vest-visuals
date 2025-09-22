8be467e025595f7c1eb77a1bdf5abb805390a3bb
Musca Mihail <muscamihaill@gmail.com>
2025-09-22 23:41:41 +0300

    refactor: fix sidebar inset overflow

--- src/components/sidebar/simple.tsx
+++ src/components/sidebar/simple.tsx
@@ -196,7 +196,7 @@ export function SimpleSidebarProvider(props: SimpleSidebarProviderProps) {
     return (
         <SidebarProvider>
             {props.sidebar}
-            <SidebarInset>
+            <SidebarInset className="overflow-hidden">
                 <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1" />
