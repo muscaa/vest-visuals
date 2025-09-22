40fb91b0bc58842eb68a8985f3543f139b775f2c
Musca Mihail <muscamihaill@gmail.com>
2025-09-22 11:41:36 +0300

    feat: added admin hook

---
+++ src/hooks/useAdmin.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+} from "@tanstack/react-query";
+import { authClient } from "@client/auth";
+
+export function useAdmin() {
+    const queryClient = useQueryClient();
+
+    const useUsers = () => useQuery({
+        queryKey: ["users"],
+        queryFn: async () => {
+            const { data, error } = await authClient.admin.listUsers({
+                query: {
+
+                },
+            });
+            if (error) return undefined;
+
+            return data.users;
+        },
+    });
+
+    return {
+        useUsers,
+    };
+}
