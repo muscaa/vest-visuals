85a6408a733672eed4f305a5da039de7810acf75
Musca Mihail <muscamihaill@gmail.com>
2025-09-22 12:50:20 +0300

    refactor: improve sql cli command

--- src/lib/server/cli/commands/sql.ts
+++ src/lib/server/cli/commands/sql.ts
@@ -1,24 +1,38 @@
 import { CliCommand } from "../core";
 import { libsql } from "@server/db";
 
-const shortcuts: Record<string, string> = {
-    tables: "SELECT name FROM sqlite_master WHERE type='table';",
+interface Shortcut {
+    command: string;
+    process?: (result: any) => any;
+}
+
+const shortcuts: Record<string, Shortcut> = {
+    tables: {
+        command: "SELECT name FROM sqlite_master WHERE type='table';",
+        process: (result) => {
+            const rows = result.rows as [string][];
+
+            return rows.flatMap((row) => row[0]);
+        },
+    },
 };
 
 export const sql: CliCommand = (program, core) => {
     program
         .command("sql")
         .argument("<string>")
-        .option("-s, --shortcut")
         .action(async (command: string, options) => {
-            const shortcut = options.shortcut;
+            let shortcut: Shortcut | undefined;
 
-            if (shortcut) {
-                command = shortcuts[command];
-                if (!command) throw new Error("Unknown shortcut");
+            if (command.startsWith(".")) {
+                shortcut = shortcuts[command.substring(1)];
+                if (!shortcut) throw new Error("Unknown shortcut");
+
+                command = shortcut.command;
             }
 
             const result = await libsql.execute(command);
-            core.out("result: ", result.toJSON());
+            const json = shortcut && shortcut.process ? shortcut.process(result) : result.toJSON();
+            core.out(json);
         });
 };
