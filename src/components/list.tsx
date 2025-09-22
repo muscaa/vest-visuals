1442790624a1799e78a18b47eb9b43103bf19667
Musca Mihail <muscamihaill@gmail.com>
2025-09-22 10:30:15 +0300

    refactor: remove list bounds

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -18,36 +18,34 @@ export function List<V>(props: ListProps<V>) {
     const isSelected = props.isSelected ?? ((value: V) => selected == value);
 
     return (
-        <div className="flex justify-center items-center size-full p-2">
-            <div className="flex flex-col size-full max-w-4xl max-h-144 gap-2">
-                {
-                    props.children && (
-                        <>
-                            <div className="flex flex-wrap gap-2">
-                                {props.children}
+        <div className="flex flex-col size-full gap-2">
+            {
+                props.children && (
+                    <>
+                        <div className="flex flex-wrap gap-2">
+                            {props.children}
+                        </div>
+                        <Separator />
+                    </>
+                )
+            }
+            <div className="flex flex-col max-h-full h-full overflow-y-auto">
+                <div className="flex flex-col grow gap-2">
+                    {
+                        props.data.map((value, index) => (
+                            <div
+                                key={index}
+                                onClick={() => setSelected(value)}
+                                className={cn(buttonVariants({
+                                    variant: "card",
+                                    size: "none",
+                                    className: `${isSelected(value) ? "border-accent-foreground dark:border-accent-foreground" : ""}`
+                                }))}
+                            >
+                                {props.entry(value, index)}
                             </div>
-                            <Separator />
-                        </>
-                    )
-                }
-                <div className="flex flex-col max-h-full h-full overflow-y-auto">
-                    <div className="flex flex-col grow gap-2">
-                        {
-                            props.data.map((value, index) => (
-                                <div
-                                    key={index}
-                                    onClick={() => setSelected(value)}
-                                    className={cn(buttonVariants({
-                                        variant: "card",
-                                        size: "none",
-                                        className: `${isSelected(value) ? "border-accent-foreground dark:border-accent-foreground" : ""}`
-                                    }))}
-                                >
-                                    {props.entry(value, index)}
-                                </div>
-                            ))
-                        }
-                    </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
