5389d008208a210cb8762d16b139d536fefa0a32
Musca Mihail <muscamihaill@gmail.com>
2025-09-23 15:37:18 +0300

    feat: added registries api validation

--- src/app/api/registries/route.ts
+++ src/app/api/registries/route.ts
@@ -9,6 +9,7 @@ import {
     updateRegistry,
     saveRegistry,
 } from "@server/registry";
+import { registrySchemas } from "@type/registries";
 
 export async function POST(request: NextRequest) {
     const admin = await isAdmin({ request });
@@ -27,12 +28,28 @@ export async function POST(request: NextRequest) {
         });
     }
 
+    const registrySchema = registrySchemas[json.name];
+    if (!registrySchema) {
+        return responseJSON<types.PostResponse>(400, {
+            success: false,
+            error: "Invalid registry name",
+        });
+    }
+
+    const validation = registrySchema.safeParse(json.value);
+    if (!validation.success) {
+        return responseJSON<types.PostResponse>(400, {
+            success: false,
+            error: "Invalid registry value",
+        });
+    }
+
     updateRegistry(json.name, (reg: any) => {
         for (const [key, _] of Object.entries(reg)) {
             reg[key] = undefined;
         }
 
-        for (const [key, value] of Object.entries(json.value)) {
+        for (const [key, value] of Object.entries(validation.data as any)) {
             reg[key] = value;
         }
     });
