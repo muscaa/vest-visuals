f9a65645e39742e942f1003964ec34a60c94b1ed
Musca Mihail <muscamihaill@gmail.com>
2025-09-23 15:33:30 +0300

    feat: added admin registries page schema dialog, minor changes & improvements

--- src/app/(private)/a/registries/page.tsx
+++ src/app/(private)/a/registries/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { CodeEditor } from "@/components/code-editor";
 import { json } from "@codemirror/lang-json";
+import { javascript } from "@codemirror/lang-javascript";
 import { toast } from "sonner";
 import {
     Select,
@@ -14,15 +15,37 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
+import {
+    Dialog,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import {
+    Registries,
+    registrySchemas,
+} from "@type/registries";
+import { zodToString } from "@shared/snippets";
 
 export default function Page() {
-    const { get, update } = useRegistries();
-    const [value, setValue] = useState("");
+    const { getRegistry, update } = useRegistries();
+    const [registry, setRegistry] = useState<string>();
+    const [value, setValue] = useState<string>();
 
     const handleSave = async () => {
+        if (!registry) {
+            toast.warning("No registry selected");
+            return;
+        }
+        if (!value) {
+            toast.warning("No value provided");
+            return;
+        }
+
         try {
             await update.mutateAsync({
-                name: "team",
+                name: registry,
                 value: JSON.parse(value),
             });
             toast.success("Registry updated");
@@ -31,10 +54,11 @@ export default function Page() {
         }
     };
 
-    const handleLoad = async (reg: string) => {
+    const handleLoad = async (reg: keyof Registries) => {
         try {
-            const data = await get.mutateAsync(reg);
+            const data = await getRegistry(reg);
             setValue(JSON.stringify(data, null, 2));
+            setRegistry(reg);
             toast.success("Registry loaded");
         } catch (error) {
             toast.error((error as Error).message);
@@ -51,16 +75,46 @@ export default function Page() {
             {
                 <div className="flex flex-col size-full gap-2">
                     <div className="flex gap-2">
-                        <Button onClick={handleSave}>
+                        <Button
+                            disabled={!registry}
+                            onClick={handleSave}
+                        >
                             Save
                         </Button>
-                        <Select onValueChange={handleLoad}>
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button
+                                    variant="secondary"
+                                    disabled={!registry}
+                                >
+                                    View Schema
+                                </Button>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Schema</DialogTitle>
+                                </DialogHeader>
+                                <CodeEditor
+                                    extensions={[javascript()]}
+                                    value={registry && zodToString(registrySchemas[registry])}
+                                    readOnly
+                                    className="max-h-[70vh]"
+                                />
+                            </DialogContent>
+                        </Dialog>
+                        <Select
+                            value={registry}
+                            onValueChange={handleLoad}
+                        >
                             <SelectTrigger>
                                 <SelectValue placeholder="Registry" />
                             </SelectTrigger>
                             <SelectContent>
-                                <SelectItem value="team">Team</SelectItem>
-                                <SelectItem value="portfolio">Portfolio</SelectItem>
+                                {
+                                    Object.entries(registrySchemas).map(([key, _], index) => (
+                                        <SelectItem key={index} value={key}>{key.toUpperCase()}</SelectItem>
+                                    ))
+                                }
                             </SelectContent>
                         </Select>
                     </div>
