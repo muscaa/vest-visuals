c099259a92a45bf345a84ee6a85b849ff56bdc26
Musca Mihail <muscamihaill@gmail.com>
2025-09-26 19:35:10 +0300

    refactor(hooks): improve upload media content mutation to bypass prod size restriction, added upload progress state

--- src/hooks/useMediaContents.ts
+++ src/hooks/useMediaContents.ts
@@ -1,18 +1,26 @@
 "use client";
 
+import { useState } from "react";
 import {
     useQuery,
     useMutation,
     useQueryClient,
 } from "@tanstack/react-query";
 import { apiClient } from "@client/http";
+import { PartialMediaContent } from "@type/media/contents";
 import * as types from "@type/api/media/contents";
 import * as types_get from "@type/api/media/contents/get";
 import * as types_upload from "@type/api/media/contents/upload";
 import * as types_remove from "@type/api/media/contents/remove";
 
+interface UploadProgress {
+    at: number;
+    max: number;
+}
+
 export function useMediaContents() {
     const queryClient = useQueryClient();
+    const [uploadProgress, setUploadProgress] = useState<UploadProgress>();
 
     const useAllMediaContents = () => useQuery({
         queryKey: ["media"],
@@ -38,16 +46,41 @@ export function useMediaContents() {
 
     const uploadMediaContents = useMutation({
         mutationFn: async (props: types_upload.PostRequest) => {
-            const formData = new FormData();
-            Array.from(props.files).forEach((file) => formData.append(types_upload.formData.files, file));
-            props.configs.forEach((config) => formData.append(types_upload.formData.configs, JSON.stringify(config)));
+            if (props.files.length != props.configs.length) throw new Error("Files & configs length don't match");
 
-            const { data } = await apiClient.postForm<types_upload.PostResponse>("/media/contents/upload", formData);
-            if (!data.success) throw new Error(data.error);
+            const values: PartialMediaContent[] = [];
 
-            await queryClient.invalidateQueries({ queryKey: ["media"] });
+            setUploadProgress({
+                at: 0,
+                max: props.files.length,
+            });
 
-            return data.values;
+            for (let i = 0; i < props.files.length; i++) {
+                try {
+                    const file = props.files[i];
+                    const config = props.configs[i];
+
+                    const formData = new FormData();
+                    formData.append(types_upload.formData.files, file);
+                    formData.append(types_upload.formData.configs, JSON.stringify(config));
+
+                    const { data } = await apiClient.postForm<types_upload.PostResponse>("/media/contents/upload", formData);
+                    if (!data.success) throw new Error(data.error);
+
+                    await queryClient.invalidateQueries({ queryKey: ["media"] });
+
+                    values.push(...data.values);
+
+                    setUploadProgress({
+                        at: i + 1,
+                        max: props.files.length,
+                    });
+                } catch (error) { }
+            }
+
+            setUploadProgress(undefined);
+
+            return values;
         },
     });
 
@@ -66,6 +99,7 @@ export function useMediaContents() {
         useAllMediaContents,
         useMediaContent,
         uploadMediaContents,
+        uploadProgress,
         removeMediaContents,
     };
 }
