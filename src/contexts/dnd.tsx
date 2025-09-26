a2101c19443e0a1759cfe20a6c86b563938bbed0
Musca Mihail <muscamihaill@gmail.com>
2025-09-26 17:59:06 +0300

    feat: finished dnd context

--- src/contexts/dnd.tsx
+++ src/contexts/dnd.tsx
@@ -2,6 +2,7 @@
 
 import {
     createContext,
+    useState,
     useEffect,
     useRef,
 } from "react";
@@ -14,16 +15,26 @@ import {
     useSensor,
     useSensors,
 } from "@dnd-kit/core";
-import { sortableKeyboardCoordinates } from "@dnd-kit/sortable";
 import {
+    sortableKeyboardCoordinates,
+    useSortable,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import {
+    DndItem,
+    DndFuncs,
     DndStartFunc,
     DndEndFunc,
-    getDndId,
+    DndOverlayFunc,
+    DndSortable,
+    getDndItem,
 } from "@client/dnd";
 
 export interface DndContext {
-    useStart: (func: DndStartFunc) => void;
-    useEnd: (func: DndEndFunc) => void;
+    useStart: (funcs: DndFuncs<DndStartFunc>) => void;
+    useEnd: (funcs: DndFuncs<DndEndFunc>) => void;
+    useOverlay: (namespace: string, overlayFunc: DndOverlayFunc) => void;
+    useItemSortable: (item: DndItem) => DndSortable;
 }
 
 export const Dnd = createContext<DndContext>(undefined as any);
@@ -40,64 +51,113 @@ export function DndContextProvider(props: DndContextProviderProps) {
         })
     );
 
-    const startFuncs = useRef<Set<DndStartFunc>>(new Set());
-    const endFuncs = useRef<Set<DndEndFunc>>(new Set());
+    const startFuncs = useRef<Set<DndFuncs<DndStartFunc>>>(new Set());
+    const endFuncs = useRef<Set<DndFuncs<DndEndFunc>>>(new Set());
+    const overlays = useRef<Record<string, DndOverlayFunc>>({});
+    const [overlay, setOverlay] = useState<React.ReactNode>();
 
-    const useStart = (func: DndStartFunc) => useEffect(() => {
-        startFuncs.current.add(func);
-
-        return () => {
-            startFuncs.current.delete(func);
-        };
-    }, []);
-
-    const useEnd = (func: DndEndFunc) => useEffect(() => {
-        endFuncs.current.add(func);
+    const useStart = (funcs: DndFuncs<DndStartFunc>) => useEffect(() => {
+        startFuncs.current.add(funcs);
 
         return () => {
-            endFuncs.current.delete(func);
+            startFuncs.current.delete(funcs);
         };
-    }, []);
+    }, [funcs]);
+
+    const useEnd = (funcs: DndFuncs<DndEndFunc>) => useEffect(() => {
+        endFuncs.current.add(funcs);
+
+        return () => {
+            endFuncs.current.delete(funcs);
+        };
+    }, [funcs]);
+
+    const useOverlay = (namespace: string, overlayFunc: DndOverlayFunc) => useEffect(() => {
+        overlays.current[namespace] = overlayFunc;
+
+        return () => {
+            delete overlays.current[namespace];
+        };
+    }, [namespace, overlayFunc]);
+
+    const useItemSortable = (item: DndItem) => {
+        const sort = useSortable({
+            id: JSON.stringify(item),
+        });
+
+        return {
+            index: sort.index,
+            newIndex: sort.newIndex,
+            isDragging: sort.isDragging,
+            setRef: sort.setNodeRef,
+            attributes: sort.attributes,
+            listeners: sort.listeners,
+            style: {
+                transform: CSS.Transform.toString(sort.transform),
+                transition: sort.transition,
+                opacity: sort.isDragging ? 0 : 1,
+            },
+        } satisfies DndSortable;
+    };
+
+    const handleStart = (active: DndItem) => {
+        for (const { pre } of startFuncs.current) {
+            pre?.(active);
+        }
+
+        const overlayFunc = overlays.current[active.namespace];
+        setOverlay(overlayFunc?.(active));
+
+        for (const { post } of startFuncs.current) {
+            post?.(active);
+        }
+    };
+
+    const handleEnd = (active: DndItem, over?: DndItem) => {
+        for (const { pre } of endFuncs.current) {
+            pre?.(active, over);
+        }
+
+        setOverlay(undefined);
+
+        for (const { post } of endFuncs.current) {
+            post?.(active, over);
+        }
+    };
 
     return (
         <Dnd.Provider
             value={{
                 useStart,
                 useEnd,
+                useOverlay,
+                useItemSortable,
             }}
         >
             <DndContext
                 sensors={sensors}
                 collisionDetection={closestCenter}
                 onDragStart={(event) => {
-                    const active = getDndId(event.active.id);
+                    const active = getDndItem(event.active.id);
 
                     if (active) {
-                        for (const func of startFuncs.current) {
-                            func(active);
-                        }
+                        handleStart(active);
                     }
                 }}
                 onDragEnd={(event) => {
-                    const active = getDndId(event.active.id);
-                    const over = getDndId(event.over?.id);
+                    const active = getDndItem(event.active.id);
+                    const over = getDndItem(event.over?.id);
 
                     if (active) {
-                        for (const func of endFuncs.current) {
-                            func(active, over);
-                        }
+                        handleEnd(active, over);
                     }
                 }}
             >
                 {props.children}
 
-                {/* <DragOverlay>
-                    {
-                        Overlay && (
-                            <Overlay />
-                        )
-                    }
-                </DragOverlay> */}
+                <DragOverlay>
+                    {overlay}
+                </DragOverlay>
             </DndContext>
         </Dnd.Provider>
     );
