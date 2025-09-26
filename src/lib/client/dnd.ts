bdf7ee42882f130a86ff55a307b4be719556812f
Musca Mihail <muscamihaill@gmail.com>
2025-09-26 17:58:24 +0300

    feat: added more dnd types, minor changes

--- src/lib/client/dnd.ts
+++ src/lib/client/dnd.ts
@@ -1,18 +1,43 @@
-import { UniqueIdentifier } from "@dnd-kit/core";
+import {
+    UniqueIdentifier,
+    DraggableAttributes,
+} from "@dnd-kit/core";
+import { SyntheticListenerMap } from "@dnd-kit/core/dist/hooks/utilities";
+export { arrayMove } from "@dnd-kit/sortable";
 
-export type DndId = {
+export type DndItem = {
     id: string;
+    namespace: string;
     type: "draggable" | "dropzone" | "sortable";
 };
 
-export function getDndId(id: UniqueIdentifier | undefined) {
+export function getDndItem(id: UniqueIdentifier | undefined) {
     if (typeof id === "string") {
         try {
-            return JSON.parse(id) as DndId;
+            return JSON.parse(id) as DndItem;
         } catch (error) { }
     }
     return undefined;
 }
 
-export type DndStartFunc = (active: DndId) => void;
-export type DndEndFunc = (active: DndId, over?: DndId) => void;
+export interface DndFuncs<T> {
+    pre?: T;
+    post?: T;
+}
+export type DndStartFunc = (active: DndItem) => void;
+export type DndEndFunc = (active: DndItem, over?: DndItem) => void;
+export type DndOverlayFunc = (item: DndItem) => React.ReactNode;
+
+export type DndSortable = {
+    index: number;
+    newIndex: number;
+    isDragging: boolean;
+    setRef: (node: HTMLElement | null) => void;
+    attributes: DraggableAttributes;
+    listeners?: SyntheticListenerMap;
+    style: {
+        transform?: string;
+        transition?: string;
+        opacity?: number;
+    };
+};
