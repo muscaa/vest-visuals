e635c26e5094596d1ed3176d5de585a2cfcfbefa
Musca Mihail <muscamihaill@gmail.com>
2026-01-20 23:56:32 +0200

    chore: remove album db schemas, services & types

--- src/lib/server/db/schema/index.ts
+++ src/lib/server/db/schema/index.ts
@@ -4,6 +4,3 @@ export * from "./mediaVariants";
 export * from "./mediaContents";
 export * from "./mediaGroups";
 export * from "./mediaCategories";
-export * from "./albumsMediaVariants";
-export * from "./albumsMediaContents";
-export * from "./albums";
