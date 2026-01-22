acf108f7d1662d2389f6893ce0a04146b0ece478
Musca Mihail <muscamihaill@gmail.com>
2026-01-22 13:34:43 +0200

    chore: added portfolio server actions

---
+++ src/actions/portfolio/index.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { ActionResponse } from "@type/http";
+import * as types from "@type/api/portfolio";
+import * as categories from "@server/portfolio/categories";
+
+export async function get(tag: string): ActionResponse<types.PortfolioEntry[]> {
+    if (!tag) {
+        return ["BAD_REQUEST", "Missing category tag"];
+    }
+
+    const result = await categories.getByTag(tag);
+    if (!result) {
+        return ["NOT_FOUND", "Category not found"];
+    }
+
+    return ["OK", result.portfolioGroups.flatMap((group) =>
+        group.portfolioMedia.flatMap((content) => {
+            const mediaVariants = content.portfolioMediaVariants;
+            if (mediaVariants.length == 0) {
+                return [];
+            }
+
+            const first = mediaVariants[0];
+            const last = mediaVariants[mediaVariants.length - 1];
+
+            return [
+                {
+                    preview: {
+                        src: first.fileUrl,
+                        width: first.info?.width,
+                        height: first.info?.height,
+                    },
+                    full: {
+                        src: last.fileUrl,
+                        width: last.info?.width,
+                        height: last.info?.height,
+                    },
+                } satisfies types.PortfolioEntry,
+            ];
+        }),
+    )];
+}
