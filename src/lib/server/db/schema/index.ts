f031909b3d60163ce16165c4c3ba69a466939cfc
Musca Mihail <muscamihaill@gmail.com>
2026-01-22 21:25:43 +0200

    chore: remove old media db schemas

--- src/lib/server/db/schema/index.ts
+++ src/lib/server/db/schema/index.ts
@@ -1,6 +1,2 @@
 export * from "./auth";
 export * from "./portfolio";
-export * from "./mediaVariants";
-export * from "./mediaContents";
-export * from "./mediaGroups";
-export * from "./mediaCategories";
