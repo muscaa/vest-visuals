be1a05f08c7429d2dee710ac6cccad58fc7810bb
Musca Mihail <muscamihaill@gmail.com>
2026-01-22 23:30:47 +0200

    feat: added shared path constants

---
+++ src/lib/shared/paths.ts
@@ -0,0 +1,26 @@
+// public
+export const HOME = "/";
+export const CONTACT = "/contact";
+export const FAQ = "/faq";
+export const PORTFOLIO = "/portfolio";
+export const PORTFOLIO_$TAG = (tag: string) => `${PORTFOLIO}/${tag}`;
+export const LOGIN = "/login";
+export const LOGIN_VERIFY = `${LOGIN}/verify`;
+export const REGISTER = "/register";
+
+// private
+export const U = "/u";
+export const U_ACCOUNT = `${U}/account`;
+export const U_PREFERENCES = `${U}/preferences`;
+export const A = "/a";
+export const A_CLI = `${A}/cli`;
+export const A_USERS = `${A}/users`;
+const A_PORTFOLIO = `${A}/portfolio`;
+export const A_PORTFOLIO_CATEGORIES = `${A_PORTFOLIO}/categories`;
+export const A_PORTFOLIO_CATEGORIES_$ID = (id: string) => `${A_PORTFOLIO_CATEGORIES}/${id}`;
+export const A_PORTFOLIO_GROUPS = `${A_PORTFOLIO}/groups`;
+export const A_PORTFOLIO_GROUPS_$ID = (id: string) => `${A_PORTFOLIO_GROUPS}/${id}`;
+export const A_PORTFOLIO_MEDIA = `${A_PORTFOLIO}/media`;
+export const A_REGISTRIES = `${A}/registries`;
+
+// api
