69970d225cd3fdeef9497f1ca184d531864f96e0
Musca Mihail <muscamihaill@gmail.com>
2026-01-27 17:34:02 +0200

    style: improve border & input colors, improve dialog footer & overlay

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -30,7 +30,7 @@ function DialogOverlay({
     return (
         <DialogPrimitive.Backdrop
             data-slot="dialog-overlay"
-            className={cn("data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 bg-black/80 duration-100 supports-backdrop-filter:backdrop-blur-xs fixed inset-0 isolate z-50", className)}
+            className={cn("data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 bg-black/10 duration-100 supports-backdrop-filter:backdrop-blur-xs fixed inset-0 isolate z-50", className)}
             {...props}
         />
     )
@@ -99,7 +99,7 @@ function DialogFooter({
         <div
             data-slot="dialog-footer"
             className={cn(
-                "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+                "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end bg-muted/50 -mx-6 -mb-6 rounded-b-xl border-t p-6",
                 className
             )}
             {...props}
