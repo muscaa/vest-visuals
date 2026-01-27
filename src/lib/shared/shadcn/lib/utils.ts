3b4d778cf3d2fccb442f6eb2a4868616422ea5a5
Musca Mihail <muscamihaill@gmail.com>
2026-01-27 16:40:27 +0200

    chore: fix headings tw merge, create separate typography utils file

--- src/lib/shared/shadcn/lib/utils.ts
+++ src/lib/shared/shadcn/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const twMerge = extendTailwindMerge<"heading">({
+    extend: {
+        classGroups: {
+            heading: ["h1", "h2", "h3", "h4", "h5", "h6", "p"],
+        },
+    },
+});
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
