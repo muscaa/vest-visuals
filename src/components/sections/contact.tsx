b2224ae2c2bcbd0fa6eb641c555bffe9bafb64d9
Musca Mihail <muscamihaill@gmail.com>
2026-01-31 00:29:26 +0200

    wip: added faq & contact sections

---
+++ src/components/sections/contact.tsx
@@ -0,0 +1,10 @@
+interface Props {
+
+}
+
+export function SectionContact(props: Props) {
+    return (
+        <section id="contact" className="flex justify-center items-center gap-8 px-2 py-16">
+        </section>
+    );
+}
