15c44ae55c2ec780f84b7e515203caed18fdd38b
Musca Mihail <muscamihaill@gmail.com>
2026-01-31 18:36:26 +0200

    fix: typography

--- src/lib/shared/shadcn/lib/utils.ts
+++ src/lib/shared/shadcn/lib/utils.ts
@@ -1,10 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { extendTailwindMerge } from "tailwind-merge";
 
-const twMerge = extendTailwindMerge<"heading">({
+const twMerge = extendTailwindMerge<"typography">({
     extend: {
         classGroups: {
-            heading: ["h1", "h2", "h3", "h4", "h5", "h6", "p"],
+            typography: ["p1", "p2", "p3", "p4", "p", "p5", "p6", "h1", "h2", "h3", "h4", "h5", "h6"],
         },
     },
 });
