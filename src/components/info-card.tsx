2e9bc5ae3342e117b35e3dbd7bbb43ccd212f83c
Musca Mihail <muscamihaill@gmail.com>
2026-02-05 19:55:12 +0200

    refactor: simplify info card, fix price section cards, added old price to price section offer card

--- src/components/info-card.tsx
+++ src/components/info-card.tsx
@@ -1,23 +1,17 @@
 "use client";
 
-import {
-    Card,
-    CardContent,
-} from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { cn } from "@shared/shadcn/lib/utils";
 
 interface Props {
     children?: React.ReactNode;
     className?: string;
-    extraClassName?: string;
 }
 
 export function InfoCard(props: Props) {
     return (
-        <Card className={cn("p-0 shadow-lg hover:shadow-xl hover:-translate-y-2 transition-all duration-300 overflow-visible", props.extraClassName)}>
-            <CardContent className={cn("relative flex p-10 w-sm", props.className)}>
-                {props.children}
-            </CardContent>
+        <Card className={cn("shadow-lg hover:shadow-xl hover:-translate-y-2 transition-all duration-300", props.className)}>
+            {props.children}
         </Card>
     );
 }
--- src/components/sections/price.tsx
+++ src/components/sections/price.tsx
@@ -2,6 +2,10 @@ import { InfoCard } from "../info-card";
 import { cn } from "@shared/shadcn/lib/utils";
 import { ButtonLink, Icon } from "../snippets";
 import { CONTACT } from "@shared/i18n";
+import {
+    CardContent,
+    CardHeader,
+} from "../ui/card";
 
 interface ExtraCardProps {
     title: string;
@@ -11,10 +15,7 @@ interface ExtraCardProps {
 
 function ExtraCard(props: ExtraCardProps) {
     return (
-        <InfoCard
-            extraClassName="border-l-4 border-l-primary rounded-lg"
-            className="flex justify-between w-xs sm:w-sm p-4"
-        >
+        <InfoCard className="flex flex-row justify-between w-xs sm:w-sm p-4 rounded-lg border-l-4 border-l-primary">
             <span>{props.title}</span>
             <div className="items-end">
                 <span className="font-extrabold tabular-nums">{props.price}</span>
@@ -31,16 +32,14 @@ interface OfferCardProps {
     currency: string;
     features: [description: string, icon?: Icon][];
     button: string;
+    oldPrice?: string;
     highlight?: string;
     accent?: boolean;
 }
 
 function OfferCard(props: OfferCardProps) {
     return (
-        <InfoCard
-            className="flex flex-col w-xs sm:w-sm"
-            extraClassName={props.accent ? "ring-2 ring-primary" : ""}
-        >
+        <InfoCard className={`relative w-xs sm:w-sm overflow-visible ${props.accent ? "ring-2 ring-primary" : ""}`}>
             {
                 props.highlight && (
                     <span className={`absolute left-1/2 -translate-x-1/2 -top-2.5 h-5 p5 px-3 bg-primary text-primary-foreground rounded-full`}>
@@ -48,38 +47,48 @@ function OfferCard(props: OfferCardProps) {
                     </span>
                 )
             }
-            <div className="flex flex-col justify-center items-center gap-2 mb-10">
+            <CardHeader className="place-items-center my-4">
                 <props.icon className="text-primary size-16" />
                 <h3 className="text-primary font-bold font-mono text-center">{props.title}</h3>
-            </div>
-            <div className="flex items-end w-full my-4">
-                <span className="h1 tabular-nums">{props.price}</span>
-                <span className="p2 font-light">{props.currency}</span>
-            </div>
-            <div className="flex flex-col gap-4 w-full my-6 font-light">
-                {
-                    props.features.map(([description, Icon], index) => (
-                        <div key={index}>
-                            {
-                                Icon && (
-                                    <Icon className="text-primary float-left mr-2" />
-                                ) || (
-                                    <span className="text-primary float-left mr-2 size-6" />
-                                )
-                            }
-                            <p>{description}</p>
-                        </div>
-                    ))
-                }
-            </div>
-            <ButtonLink
-                href={CONTACT()}
-                variant={props.accent ? "default" : "secondary"}
-                size="lg"
-                className="mt-6 w-full"
-            >
-                {props.button}
-            </ButtonLink>
+            </CardHeader>
+            <CardContent>
+                <div className="flex items-end w-full my-4">
+                    {
+                        props.oldPrice && (
+                            <>
+                                <span className="h1 tabular-nums text-muted-foreground line-through">{props.oldPrice}</span>
+                                <span className="p2 font-light text-muted-foreground line-through mr-4">{props.currency}</span>
+                            </>
+                        )
+                    }
+                    <span className="h1 tabular-nums">{props.price}</span>
+                    <span className="p2 font-light">{props.currency}</span>
+                </div>
+                <div className="flex flex-col gap-4 w-full my-6 font-light">
+                    {
+                        props.features.map(([description, Icon], index) => (
+                            <div key={index}>
+                                {
+                                    Icon && (
+                                        <Icon className="text-primary float-left mr-2" />
+                                    ) || (
+                                        <span className="text-primary float-left mr-2 size-6" />
+                                    )
+                                }
+                                <p>{description}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+                <ButtonLink
+                    href={CONTACT()}
+                    variant={props.accent ? "default" : "secondary"}
+                    size="lg"
+                    className="mt-6 w-full"
+                >
+                    {props.button}
+                </ButtonLink>
+            </CardContent>
         </InfoCard>
     );
 }
