972652d982c866bd271a0c4ff6bb9c7dc12b4d6d
Musca Mihail <muscamihaill@gmail.com>
2026-02-05 18:39:36 +0200

    fix: sidebar breadcrumbs separator

--- src/components/layout/providers/sidebar.tsx
+++ src/components/layout/providers/sidebar.tsx
@@ -24,7 +24,7 @@ function LayoutHeader() {
                         <>
                             <Separator
                                 orientation="vertical"
-                                className="mr-2 data-[orientation=vertical]:h-4"
+                                className="mr-2"
                             />
                             <Breadcrumbs
                                 list={breadcrumbs}
