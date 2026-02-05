92f9b9b3f62384dab5d64c7c53e1964b4aec2536
Musca Mihail <muscamihaill@gmail.com>
2026-02-05 17:54:50 +0200

    feat: finished team section, added badge ui component, improved text link

--- src/components/sections/team.tsx
+++ src/components/sections/team.tsx
@@ -3,102 +3,76 @@ import {
     SiInstagram,
 } from "@icons-pack/react-simple-icons";
 import Linkedin from "@/components/svg/linkedin";
-import { ButtonLink, IconLink } from "@/components/snippets";
-import { Separator } from "@/components/ui/separator";
-import { TextLink } from "@/components/ui/text-link";
 import { Img } from "@/components/snippets";
 import { TeamMember } from "@type/registries/team-members";
 import { useRegistries } from "@/hooks/useRegistries";
 import { cn } from "@shared/shadcn/lib/utils";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+} from "../ui/card";
+import { TextLink } from "../ui/text-link";
+import { Separator } from "../ui/separator";
 
 function Member(props: TeamMember) {
     return (
-        <div
-            className="
-                bg-primary/10 flex w-full group
-                not-sm:flex-col
-                sm:even:flex-row-reverse
-            "
-        >
+        <Card className="shadow-lg hover:shadow-xl hover:-translate-y-2 transition-all duration-300 w-xs sm:w-sm">
             <Img
                 src={props.image}
                 alt={props.name}
-                width={512}
-                height={512}
-                className="size-64 lg:size-96 border-4 border-primary shrink-0"
             />
-            <div
-                className="
-                    flex flex-col grow
-                    sm:p-4 sm:gap-4
-                    lg:p-8 lg:gap-8
-                "
-            >
-                <div
-                    className="
-                        bg-primary theme-dark flex justify-between items-center
-                        shadow-lg hover:shadow-xl hover:-translate-y-2 transition-all duration-300
-                        sm:group-even:flex-row-reverse
-                        sm:h-10 sm:px-4 sm:group-odd:pl-8 sm:group-even:pr-8 sm:group-odd:-ml-8 sm:group-even:-mr-8
-                        lg:h-16 lg:px-8 lg:group-odd:pl-16 lg:group-even:pr-16 lg:group-odd:-ml-16 lg:group-even:-mr-16
-                    "
-                >
-                    <h3 className="font-mono text-center h1">{props.name}</h3>
+            <CardHeader>
+                <div className="flex justify-between items-center">
+                    <h3 className="font-mono h1">{props.name}</h3>
                     <div className="flex">
-                        <ButtonLink
-                            href="#"
-                            size="icon-responsive"
-                        >
-                            <SiInstagram />
-                        </ButtonLink>
-                        <ButtonLink
-                            href="#"
-                            size="icon-responsive"
-                        >
-                            <SiFacebook />
-                        </ButtonLink>
-                        <ButtonLink
-                            href="#"
-                            size="icon-responsive"
-                        >
-                            <Linkedin />
-                        </ButtonLink>
+                        {
+                            props.socials.instagram && (
+                                <TextLink
+                                    href={props.socials.instagram}
+                                    variant="ghost"
+                                    size="icon-responsive"
+                                >
+                                    <SiInstagram />
+                                </TextLink>
+                            )
+                        }
+                        {
+                            props.socials.facebook && (
+                                <TextLink
+                                    href={props.socials.facebook}
+                                    variant="ghost"
+                                    size="icon-responsive"
+                                >
+                                    <SiFacebook />
+                                </TextLink>
+                            )
+                        }
+                        {
+                            props.socials.linkedin && (
+                                <TextLink
+                                    href={props.socials.linkedin}
+                                    variant="ghost"
+                                    size="icon-responsive"
+                                >
+                                    <Linkedin />
+                                </TextLink>
+                            )
+                        }
                     </div>
                 </div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mollis tellus vitae
-                    vestibulum porta. Aliquam tempor gravida feugiat. Nulla ex mi, tincidunt eget ante
-                    consectetur, iaculis sagittis turpis. Donec mattis bibendum tellus eu fermentum.
-                    Curabitur et lacus ullamcorper ex aliquam congue. In fringilla rutrum lectus, et
-                    faucibus tortor placerat et.
-                </p>
-            </div>
-            {/* <div className="flex flex-col justify-center items-center gap-1">
-                <h2>{props.name}</h2>
-                <div className="flex flex-wrap justify-center items-center gap-2">
-                    {
-                        props.roles.map((role, index) => (
-                            <div key={index} className="flex h-6 items-center gap-2">
-                                <h4 className="text-muted-foreground">{role}</h4>
-                                {
-                                    index < props.roles.length - 1 && (
-                                        <Separator orientation="vertical" />
-                                    )
-                                }
-                            </div>
-                        ))
-                    }
-                </div>
-                <TextLink href={`mailto:${props.email}`} className="h4">
+                <TextLink
+                    href={`mailto:${props.email}`}
+                    className="w-min"
+                >
                     {props.email}
                 </TextLink>
-            </div>
-            <div className="flex items-center justify-center gap-4">
-                {props.socials.instagram && <IconLink href={props.socials.instagram} icon={SiInstagram} />}
-                {props.socials.facebook && <IconLink href={props.socials.facebook} icon={SiFacebook} />}
-                {props.socials.linkedin && <IconLink href={props.socials.linkedin} icon={Linkedin} />}
-            </div> */}
-        </div>
+                <Separator />
+            </CardHeader>
+            <CardContent className="flex flex-col gap-4">
+                <span className="text-muted-foreground">{props.roles.join(" | ")}</span>
+            </CardContent>
+        </Card>
     );
 }
 
@@ -116,7 +90,7 @@ export function SectionTeam(props: SectionTeamProps) {
             className={cn("flex flex-col justify-center items-center gap-8 p-8 w-full", props.className)}
         >
             <h2 className="font-mono text-center">Echipa</h2>
-            <div className="flex flex-col max-w-6xl w-full justify-center gap-8">
+            <div className="flex flex-wrap max-w-6xl w-full justify-evenly gap-8">
                 {
                     data && data.map((member, index) => (
                         <Member
--- src/components/ui/text-link.tsx
+++ src/components/ui/text-link.tsx
@@ -5,7 +5,7 @@ import { cn } from "@shared/shadcn/lib/utils"
 import { Link } from "@shared/i18n"
 
 const textLinkVariants = cva(
-    "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 rounded-lg border border-transparent bg-clip-padding p focus-visible:ring-[3px] aria-invalid:ring-[3px] [&_svg:not([class*='size-'])]:size-4 transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none",
+    "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 rounded-lg border border-transparent bg-clip-padding p focus-visible:ring-[3px] aria-invalid:ring-[3px] [&_svg:not([class*='size-'])]:size-4 inline-flex items-center justify-center transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none",
     {
         variants: {
             variant: {
@@ -13,14 +13,17 @@ const textLinkVariants = cva(
                 ghost: "underline-offset-4 hover:underline hover:text-accent-foreground",
             },
             size: {
-                default: "h-9 gap-1.5 px-3 has-data-[icon=inline-end]:pr-2.5 has-data-[icon=inline-start]:pl-2.5",
+                // default: "h-9 gap-1.5 px-3 has-data-[icon=inline-end]:pr-2.5 has-data-[icon=inline-start]:pl-2.5",
                 xs: "h-6 gap-1 px-2.5 p6 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 [&_svg:not([class*='size-'])]:size-3",
-                sm: "h-8 gap-1 px-3 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
-                lg: "h-10 gap-1.5 px-4 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3",
-                icon: "size-9",
+                sm: "h-8 gap-1 px-3 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 size-8 [&_svg:not([class*='size-'])]:size-4",
+                default: "h-10 gap-1.5 px-4 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3 [&_svg:not([class*='size-'])]:size-6",
+                lg: "h-12 gap-2 px-4 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3 [&_svg:not([class*='size-'])]:size-8",
+                // icon: "size-9",
                 "icon-xs": "size-6 [&_svg:not([class*='size-'])]:size-3",
-                "icon-sm": "size-8",
-                "icon-lg": "size-10",
+                "icon-sm": "size-8 [&_svg:not([class*='size-'])]:size-4",
+                icon: "size-10 [&_svg:not([class*='size-'])]:size-6",
+                "icon-lg": "size-12 [&_svg:not([class*='size-'])]:size-8",
+                "icon-responsive": "size-10 [&_svg:not([class*='size-'])]:size-6 lg:size-12 lg:[&_svg:not([class*='size-'])]:size-8",
                 none: "",
             },
         },
