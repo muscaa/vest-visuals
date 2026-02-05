8638d1e6d12955c717234e0fbba2d3c467d5e60a
Musca Mihail <muscamihaill@gmail.com>
2026-02-05 21:10:36 +0200

    chore: remove portfolio page, fix not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default async function NotFound() {
 
     return (
         <NavbarLayoutProvider>
-            <div className="flex flex-col justify-center items-center text-center h-full gap-4">
+            <div className="flex flex-col justify-center items-center text-center gap-8 p-8 w-full min-h-screen-no-nav">
                 <h1 className="h0">404</h1>
                 <h2>{t("not-found")}</h2>
             </div>
