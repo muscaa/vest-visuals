4dc409b2b7ed3a357e06ff6ed864529257bb31c5
Musca Mihail <muscamihaill@gmail.com>
2026-02-19 20:20:57 +0200

    feat: added albums contents action & hook

---
+++ src/actions/albums/contents.ts
@@ -0,0 +1,92 @@
+"use server";
+
+import { ActionResponse } from "@type/http";
+import { isAdmin } from "@server/auth/permissions";
+import * as types from "@type/albums/contents";
+import * as contents from "@server/albums/contents";
+
+export async function get(id: string): ActionResponse<types.AlbumsContent> {
+    const admin = await isAdmin({ next: true });
+    if (!admin) {
+        return ["UNAUTHORIZED", "Unauthorized"];
+    }
+
+    if (!id) {
+        return ["BAD_REQUEST", "Missing content ID"];
+    }
+
+    const result = await contents.get(id);
+    if (!result) {
+        return ["NOT_FOUND", "Content not found"];
+    }
+
+    return ["OK", result];
+}
+
+export async function getAll(): ActionResponse<types.PartialAlbumsContent[]> {
+    const admin = await isAdmin({ next: true });
+    if (!admin) {
+        return ["UNAUTHORIZED", "Unauthorized"];
+    }
+
+    const result = await contents.getAllPartial();
+    if (!result) {
+        return ["INTERNAL_SERVER_ERROR", "Could not retrieve contents"];
+    }
+
+    return ["OK", result];
+}
+
+export async function create(value: types.CreateProps): ActionResponse<types.PartialAlbumsContent> {
+    const admin = await isAdmin({ next: true });
+    if (!admin) {
+        return ["UNAUTHORIZED", "Unauthorized"];
+    }
+
+    if (!value) {
+        return ["BAD_REQUEST", "Missing content properties"];
+    }
+
+    const result = await contents.create(value);
+    if (!result) {
+        return ["INTERNAL_SERVER_ERROR", "Could not create content"];
+    }
+
+    return ["OK", result];
+}
+
+export async function update(id: string, value: types.UpdateProps): ActionResponse<void> {
+    const admin = await isAdmin({ next: true });
+    if (!admin) {
+        return ["UNAUTHORIZED", "Unauthorized"];
+    }
+
+    if (!id || !value) {
+        return ["BAD_REQUEST", "Missing content ID or properties"];
+    }
+
+    const result = await contents.update(id, value);
+    if (!result) {
+        return ["INTERNAL_SERVER_ERROR", "Could not update content"];
+    }
+
+    return ["OK"];
+}
+
+export async function remove(id: string): ActionResponse<void> {
+    const admin = await isAdmin({ next: true });
+    if (!admin) {
+        return ["UNAUTHORIZED", "Unauthorized"];
+    }
+
+    if (!id) {
+        return ["BAD_REQUEST", "Missing content ID"];
+    }
+
+    const result = contents.remove(id);
+    if (!result) {
+        return ["NOT_FOUND", "Content not found"];
+    }
+
+    return ["OK"];
+}
---
+++ src/hooks/albums/useAlbumsContents.ts
@@ -0,0 +1,74 @@
+"use client";
+
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+} from "@tanstack/react-query";
+import * as types from "@type/albums/contents";
+import * as contents from "@/actions/albums/contents";
+
+export function useAlbumsContents() {
+    const queryClient = useQueryClient();
+
+    const useAllAlbumsContents = () => useQuery({
+        queryKey: ["albums", "contents"],
+        queryFn: async () => {
+            const [status, result] = await contents.getAll();
+            if (status !== "OK") return [];
+
+            return result || [];
+        },
+    });
+
+    const useAlbumsContent = (id: string) => useQuery({
+        queryKey: ["albums", id],
+        queryFn: async () => {
+            const [status, result] = await contents.get(id);
+            if (status !== "OK") return null;
+
+            return result || null;
+        },
+    });
+
+    const createAlbumsContent = useMutation({
+        mutationFn: async (props: types.CreateProps) => {
+            const [status, result] = await contents.create(props);
+            if (status !== "OK") throw new Error(result as string);
+
+            await queryClient.invalidateQueries({ queryKey: ["albums"] });
+
+            return result;
+        },
+    });
+
+    const updateAlbumsContent = useMutation({
+        mutationFn: async (props: { id: string; value: types.UpdateProps; }) => {
+            const [status, result] = await contents.update(props.id, props.value);
+            if (status !== "OK") throw new Error(result as string);
+
+            await queryClient.invalidateQueries({ queryKey: ["albums"] });
+
+            return true;
+        },
+    });
+
+    const removeAlbumsContent = useMutation({
+        mutationFn: async (id: string) => {
+            const [status, result] = await contents.remove(id);
+            if (status !== "OK") throw new Error(result as string);
+
+            await queryClient.invalidateQueries({ queryKey: ["albums"] });
+
+            return true;
+        },
+    });
+
+    return {
+        useAllAlbumsContents,
+        useAlbumsContent,
+        createAlbumsContent,
+        updateAlbumsContent,
+        removeAlbumsContent,
+    };
+}
