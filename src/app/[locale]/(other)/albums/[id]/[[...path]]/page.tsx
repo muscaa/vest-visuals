37e03130c6a373f676e0fd34c1e5c4d68fa65980
Musca Mihail <muscamihaill@gmail.com>
2026-02-24 19:42:09 +0200

    chore: remove album page unused code

--- src/app/[locale]/(other)/albums/[id]/[[...path]]/page.tsx
+++ src/app/[locale]/(other)/albums/[id]/[[...path]]/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { AlbumsContentsList } from "@/components/lists/albums/albums-contents";
-import {
-    Loading,
-    NotFound,
-} from "@/components/status";
-import { useAlbumsContents } from "@/hooks/albums/useAlbumsContents";
 import { useMemo } from "react";
 import { MediaWaterfall } from "@/components/media/media-waterfall";
 import { getPaginated } from "@/actions/albums";
@@ -14,9 +8,6 @@ import { getPaginated } from "@/actions/albums";
 export default function Page() {
     const params = useParams<{ id: string; path?: string[]; }>();
     const path = useMemo(() => params.path ? params.path.map((value) => decodeURIComponent(value)) : undefined, [params.path]);
-
-    // const { useAlbumsContentByPath } = useAlbumsContents();
-    // const { data } = useAlbumsContentByPath(params.id, path);
 
     const handleNextData = async (offset: number, limit: number) => {
         const [status, result] = await getPaginated(offset, limit, params.id, path);
@@ -26,19 +17,8 @@ export default function Page() {
     };
 
     return (
-        // data && (
-            <MediaWaterfall
-                nextData={handleNextData}
-            />
-            // <AlbumsContentsList
-            //     data={data}
-            //     albumId={params.id}
-            //     path={path}
-            // />
-        // ) || data === null && (
-        //     <NotFound />
-        // ) || (
-        //     <Loading />
-        // )
+        <MediaWaterfall
+            nextData={handleNextData}
+        />
     );
 }
