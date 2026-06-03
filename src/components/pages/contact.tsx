98020dda1a5a42a2d6c48b0943e7f33de234345e
Musca Mihail <muscamihaill@gmail.com>
2026-06-03 19:43:29 +0300

    feat: added polygon scatter background

--- src/components/pages/contact.tsx
+++ src/components/pages/contact.tsx
@@ -5,7 +5,10 @@ interface Props {
 export function ContactPage(props: Props) {
     return (
         <>
-            hello world
+            <main className="flex flex-col min-h-screen-no-nav">
+                <div className="polygon-scatter-background" />
+                hello world
+            </main>
         </>
     );
 }
