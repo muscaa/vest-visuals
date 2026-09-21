632e3122baa32d6c40e22abe2b2da2fdc8c67e56
Musca Mihail <muscamihaill@gmail.com>
2026-09-21 19:43:24 +0300

    refactor: format vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
     server: {
@@ -14,9 +14,9 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         tanstackStart({
-            srcDirectory: 'src',
+            srcDirectory: "src",
         }),
         viteReact(),
         nitro(),
     ],
-})
+});
