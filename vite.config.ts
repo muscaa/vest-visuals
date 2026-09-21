9d7e7fa29b04101849fbaf9270ae499f5a093742
Musca Mihail <muscamihaill@gmail.com>
2026-09-21 19:17:52 +0300

    refactor: format vite.config.ts, tsconfig.json, package.json, remove next js gitignore entries, add tanstack gitignore entries, clear .env.example

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    tsconfigPaths: true,
-  },
-  plugins: [
-    tailwindcss(),
-    tanstackStart({
-      srcDirectory: 'src',
-    }),
-    viteReact(),
-    nitro(),
-  ],
+    server: {
+        port: 3000,
+    },
+    resolve: {
+        tsconfigPaths: true,
+    },
+    plugins: [
+        tailwindcss(),
+        tanstackStart({
+            srcDirectory: 'src',
+        }),
+        viteReact(),
+        nitro(),
+    ],
 })
