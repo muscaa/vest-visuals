500510483c802148d83242af4c0ca49c254b1858
Musca Mihail <muscamihaill@gmail.com>
2026-09-21 22:18:17 +0300

    feat: added eslint & prettier

---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "prettier";
+
+export default {
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    tabWidth: 4,
+    endOfLine: "auto",
+} satisfies Config;
