22ec4953c6577afdbf9f1c6e8eae0ba5847fab6f
Musca Mihail <muscamihaill@gmail.com>
2026-09-23 16:15:15 +0300

    chore: added more auth functions

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -2,9 +2,25 @@ import { auth } from "@server/auth";
 import { createServerFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
 
-export const getSession = createServerFn().handler(() =>
-    auth.api.getSession({ headers: getRequestHeaders() }),
-);
+export const getSession = createServerFn({ method: "GET" })
+    .handler(async () => {
+        const headers = getRequestHeaders();
+        const session = await auth.api.getSession({ headers });
+
+        return session;
+    });
+
+export const ensureSession = createServerFn({ method: "GET" })
+    .handler(async () => {
+        const headers = getRequestHeaders();
+        const session = await auth.api.getSession({ headers });
+
+        if (!session) {
+            throw new Error("Unauthorized");
+        }
+
+        return session;
+    });
 
 export const login = createServerFn({ method: "POST" })
     .validator((data: FormData) => {
